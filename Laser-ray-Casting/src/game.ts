699173c845b55d4c264c9563dd236a0201fff336
Leandro Mendoza <leandro@decentraland.org>
2022-11-09 11:46:47 -0300

    add laser raycast and smoke scenes

---
+++ Laser-ray-Casting/src/game.ts
@@ -0,0 +1,69 @@
+import { boxesCount, defaultMaterial, MovingCube, Ray, rayDistance, rayMaterial } from './definitions'
+import movingCubesSystem from './modules/movingCubes'
+import raycastSystem from './modules/ray'
+
+function setup() {
+  // laser turret
+  const turretBase = engine.addEntity()
+  GltfContainer.create(turretBase, { src: 'models/TurretBase_01.glb' })
+  Transform.create(turretBase, {
+    position: Vector3.create(16, 0, 2)
+  })
+
+  const turret = engine.addEntity()
+  GltfContainer.create(turret, { src: 'models/Turret_01.glb' })
+  Transform.create(turret, {
+    position: Vector3.create(16, 1, 2)
+  })
+  Ray.create(turret, {
+    power: 1000
+  })
+
+  // Ray
+  const rayCube = engine.addEntity()
+  Transform.create(rayCube, {
+    position: Vector3.create(16, 1.645, 0)
+  })
+
+  const rayCubeObject = engine.addEntity()
+  MeshRenderer.create(rayCubeObject, {
+    mesh: { $case: 'box', box: { uvs: [] } }
+  })
+  Transform.create(rayCubeObject, {
+    parent: rayCube,
+    scale: Vector3.create(0.1, 0.1, rayDistance),
+    position: Vector3.create(0, 0, rayDistance / 2 + 3)
+  })
+  Material.create(rayCubeObject, rayMaterial)
+
+  for (let i = 0; i < boxesCount; i++) {
+    const cube = engine.addEntity()
+    Transform.create(cube, {
+      position: Vector3.create(2 + i * 3, 1.5, 16)
+    })
+    MeshRenderer.create(cube, {
+      mesh: { $case: 'box', box: { uvs: [] } }
+    })
+    MeshCollider.create(cube, {
+      mesh: { $case: 'box', box: { uvs: [] } }
+    })
+    Material.create(cube, defaultMaterial)
+    MovingCube.create(cube)
+  }
+
+  const floor = engine.addEntity()
+  GltfContainer.create(floor, { src: 'models/Floor_sciFi_03.glb' })
+  Transform.create(floor, {
+    position: Vector3.create(16, 0, 16),
+    scale: Vector3.create(2, 0, 2)
+  })
+
+  engine.addSystem(movingCubesSystem)
+  engine.addSystem(raycastSystem)
+
+  Ray.create(engine.CameraEntity, {
+    power: 1000
+  })
+}
+
+setup()
---
+++ Smoke/src/modules/smokeSource.ts
@@ -0,0 +1,47 @@
+import { smokeMaterial, SmokeParticle, SmokeSource } from '../definitions'
+
+function spawnSmokePuff(entity: Entity, parent: Entity) {
+  const newVel: Vector3 = {
+    x: (Math.random() - Math.random()) / 6,
+    y: Math.random() / 2 + 0.1,
+    z: (Math.random() - Math.random()) / 6
+  }
+  const size = Math.random() / 2 + 0.2
+
+  MeshRenderer.createOrReplace(entity, { mesh: { $case: 'plane', plane: { uvs: [] } } })
+  Material.createOrReplace(entity, smokeMaterial)
+
+  Transform.createOrReplace(entity, {
+    scale: Vector3.create(size, size, size),
+    parent
+  })
+
+  Billboard.createOrReplace(entity, { billboardMode: BillboardMode.BM_Y_AXE })
+
+  SmokeParticle.createOrReplace(entity, {
+    velocity: newVel,
+    visible: true
+  })
+}
+
+export default function throwSmokeSystem(dt: number) {
+  for (const [entity] of engine.getEntitiesWith(SmokeSource)) {
+    const smokeSource = SmokeSource.getMutable(entity)
+    smokeSource.nextSmoke -= dt
+    if (smokeSource.nextSmoke < 0) {
+      smokeSource.nextSmoke = smokeSource.smokeInterval
+
+      const smokeParticles = Array.from(engine.getEntitiesWith(SmokeParticle))
+
+      if (smokeParticles.length < smokeSource.particleCount) {
+        spawnSmokePuff(engine.addEntity(), entity)
+      } else {
+        const freeParticle = smokeParticles.find(([_, p]) => p.visible === false)
+        if (freeParticle) {
+          const [particleEntity] = freeParticle
+          spawnSmokePuff(particleEntity, entity)
+        }
+      }
+    }
+  }
+}
