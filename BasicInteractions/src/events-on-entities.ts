e740d2015914b3c8cd02c116ab649f472f71176b
nearnshaw <earnshaw.nico@gmail.com>
2022-12-29 10:31:44 -0300

    friendlier syntax in basic interactions

--- BasicInteractions/src/events-on-entities.ts
+++ BasicInteractions/src/events-on-entities.ts
@@ -1,112 +1,98 @@
 // Pointer Down Cube
 
-import { PointerHoverFeedback, PointerEventType, InputAction, engine, inputSystem } from '@dcl/sdk/ecs'
+import { PointerHoverFeedback, PointerEventType, InputAction, engine, inputSystem, pointerEventsSystem } from '@dcl/sdk/ecs'
 import { Vector3 } from '@dcl/sdk/math'
 import { PainterComponent } from './painter'
 import { createMesh } from './utils'
 
 export function setupEventOnEntities() {
   const pointerDownCube = createMesh(Vector3.create(2, 1, 4), 'Pointer down', 1, false)
-  PointerHoverFeedback.create(pointerDownCube, {
-    pointerEvents: [
-      {
-        eventType: PointerEventType.PET_DOWN,
-        eventInfo: {
-          button: InputAction.IA_POINTER,
-          hoverText: 'Activate'
-        }
-      }
-    ]
-  })
+  
+  pointerEventsSystem.onPointerDown(
+	pointerDownCube,
+	function (cmd) {
+		PainterComponent.createOrReplace(pointerDownCube)
+	},
+	{
+		button: InputAction.IA_POINTER,
+		hoverText: 'Activate'
+	}
+  ) 
+
 
   // Pointer Up Cube
 
   const pointerUpCube = createMesh(Vector3.create(2, 1, 6), 'Pointer up', 1, false)
-  PointerHoverFeedback.create(pointerUpCube, {
-    pointerEvents: [
-      {
-        eventType: PointerEventType.PET_UP,
-        eventInfo: {
-          button: InputAction.IA_PRIMARY,
-          hoverText: 'Activate'
-        }
-      }
-    ]
-  })
+  
+  
+  pointerEventsSystem.onPointerUp(
+	pointerUpCube,
+	function (cmd) {
+		PainterComponent.createOrReplace(pointerUpCube)
+	},
+	{
+		button: InputAction.IA_POINTER,
+		hoverText: 'Activate'
+	}
+  ) 
+ 
 
   //  Primary Down Cube (while pointing)
   const primaryDownCube = createMesh(Vector3.create(8, 1, 12), 'Primary down', 1, false)
-  PointerHoverFeedback.create(primaryDownCube, {
-    pointerEvents: [
-      {
-        eventType: PointerEventType.PET_DOWN,
-        eventInfo: {
-          button: InputAction.IA_PRIMARY,
-          hoverText: 'Activate'
-        }
-      }
-    ]
-  })
+ 
+  pointerEventsSystem.onPointerDown(
+	primaryDownCube,
+	function (cmd) {
+		PainterComponent.createOrReplace(primaryDownCube)
+	},
+	{
+		button: InputAction.IA_PRIMARY,
+		hoverText: 'Activate'
+	}
+  ) 
+ 
   // Primary Up Cube
   const primaryUpCube = createMesh(Vector3.create(10, 1, 12), 'Primary up', 1, false)
-  PointerHoverFeedback.create(primaryUpCube, {
-    pointerEvents: [
-      {
-        eventType: PointerEventType.PET_UP,
-        eventInfo: {
-          button: InputAction.IA_PRIMARY,
-          hoverText: 'Activate'
-        }
-      }
-    ]
-  })
+ 
+  pointerEventsSystem.onPointerUp(
+	primaryUpCube,
+	function (cmd) {
+		PainterComponent.createOrReplace(primaryUpCube)
+	},
+	{
+		button: InputAction.IA_PRIMARY,
+		hoverText: 'Activate'
+	}
+  ) 
+ 
+
 
   // Secondary Down Cube
   const secondaryDownCube = createMesh(Vector3.create(12, 1, 12), 'Secondary down', 1, false)
-  PointerHoverFeedback.create(secondaryDownCube, {
-    pointerEvents: [
-      {
-        eventType: PointerEventType.PET_DOWN,
-        eventInfo: {
-          button: InputAction.IA_SECONDARY,
-          hoverText: 'Activate'
-        }
-      }
-    ]
-  })
+ 
+  pointerEventsSystem.onPointerDown(
+	secondaryDownCube,
+	function (cmd) {
+		PainterComponent.createOrReplace(secondaryDownCube)
+	},
+	{
+		button: InputAction.IA_SECONDARY,
+		hoverText: 'Activate'
+	}
+  ) 
+
 
   // Secondary Up Cube
   const secondaryUpCube = createMesh(Vector3.create(14, 1, 12), 'Secondary up', 1, false)
-  PointerHoverFeedback.create(secondaryUpCube, {
-    pointerEvents: [
-      {
-        eventType: PointerEventType.PET_UP,
-        eventInfo: {
-          button: InputAction.IA_SECONDARY,
-          hoverText: 'Activate'
-        }
-      }
-    ]
-  })
-
-  engine.addSystem(() => {
-    if (inputSystem.isTriggered(InputAction.IA_POINTER, PointerEventType.PET_DOWN, pointerDownCube)) {
-      PainterComponent.createOrReplace(pointerDownCube)
-    }
-    if (inputSystem.isTriggered(InputAction.IA_POINTER, PointerEventType.PET_UP, pointerUpCube)) {
-      PainterComponent.createOrReplace(pointerUpCube)
-    }
-    if (inputSystem.isTriggered(InputAction.IA_PRIMARY, PointerEventType.PET_DOWN, primaryDownCube)) {
-      PainterComponent.createOrReplace(primaryDownCube)
-    }
-    if (inputSystem.isTriggered(InputAction.IA_PRIMARY, PointerEventType.PET_UP, primaryUpCube)) {
-      PainterComponent.createOrReplace(primaryUpCube)
-    }
-    if (inputSystem.isTriggered(InputAction.IA_SECONDARY, PointerEventType.PET_DOWN, secondaryDownCube)) {
-      PainterComponent.createOrReplace(secondaryDownCube)
-    }
-    if (inputSystem.isTriggered(InputAction.IA_SECONDARY, PointerEventType.PET_UP, secondaryUpCube)) {
-      PainterComponent.createOrReplace(secondaryUpCube)
-    }
-  })
+ 
+  pointerEventsSystem.onPointerUp(
+	secondaryUpCube,
+	function (cmd) {
+		PainterComponent.createOrReplace(secondaryUpCube)
+	},
+	{
+		button: InputAction.IA_SECONDARY,
+		hoverText: 'Activate'
+	}
+  ) 
 }
