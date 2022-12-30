f3c25b84da95de573f66b4146fb6c8a0de1ba38f
menduz <github@menduz.com>
2022-12-30 13:27:06 -0300

    fix enemy spawner

--- enemy-spawner/src/systems/clickable.ts
+++ enemy-spawner/src/systems/clickable.ts
@@ -1,47 +1,49 @@
-import { Entity, engine, PointerHoverFeedback, InputAction, PointerEventType, inputSystem, AudioSource, AvatarAttach } from '@dcl/sdk/ecs'
-
-
+import {
+  Entity,
+  engine,
+  PointerHoverFeedback,
+  InputAction,
+  PointerEventType,
+  inputSystem,
+  AudioSource,
+  AvatarAttach,
+  AvatarAnchorPointType,
+} from "@dcl/sdk/ecs"
 
 export function clickedSystem() {
+  if (inputSystem.getInputCommand(InputAction.IA_POINTER, PointerEventType.PET_DOWN)) {
+    const blasterSound = AudioSource.getMutable(soundPlayer)
+    blasterSound.playing = true
 
-	if (inputSystem.getInputCommand(InputAction.IA_POINTER, PointerEventType.PET_DOWN)) {
-		const blasterSound = AudioSource.getMutable(soundPlayer)
-		blasterSound.playing = true
-
-		for (const [entity] of engine.getEntitiesWith(EnemyShip)) {
-			if(inputSystem.isTriggered(InputAction.IA_POINTER, PointerEventType.PET_DOWN, entity)){
-				destroyEnemy(entity)
-				console.log("HIT ENEMY")
-			}
-		}
-	}
- 
+    for (const [entity] of engine.getEntitiesWith(EnemyShip)) {
+      if (inputSystem.isTriggered(InputAction.IA_POINTER, PointerEventType.PET_DOWN, entity)) {
+        destroyEnemy(entity)
+        console.log("HIT ENEMY")
+      }
+    }
+  }
 }
-
-
-
-
 
 const soundPlayer = engine.addEntity()
 AudioSource.create(soundPlayer, {
-	audioClipUrl: 'sounds/blaster.mp3',
-    playing: false,
-	loop: false,
-	volume: 0.5
+  audioClipUrl: "sounds/blaster.mp3",
+  playing: false,
+  loop: false,
+  volume: 0.5,
 })
 attachEntity(soundPlayer)
 
-
 import { getUserData } from "~system/UserIdentity"
-import { EnemyShip } from '../components/customComponents'
-import { destroyEnemy } from '../enemy'
+import { EnemyShip } from "../components/customComponents"
+import { destroyEnemy } from "../enemy"
 
-async function attachEntity (entity: Entity){
+async function attachEntity(entity: Entity) {
   let userData = await getUserData({})
-  if(!userData.data) return
+  if (!userData.data) return
   console.log(userData.data.userId)
 
   AvatarAttach.create(entity, {
-	avatarId: userData.data.userId
+    anchorPointId: AvatarAnchorPointType.AAPT_POSITION,
+    avatarId: userData.data.userId
   })
 }
--- enemy-spawner/src/systems/expire.ts
+++ enemy-spawner/src/systems/expire.ts
@@ -2,12 +2,12 @@ import { engine } from "@dcl/sdk/ecs"
 import { Expire } from "../components/customComponents"
 
 export function expire(dt: number) {
-	for (const [entity] of engine.getEntitiesWith(Expire)) {
-	  const expire = Expire.getMutable(entity)
+  for (const [entity] of engine.getEntitiesWith(Expire)) {
+    const expire = Expire.getMutable(entity)
 
-	  expire.timeLeft -= dt
-	  if(expire.timeLeft < 0){
-		engine.removeEntity(entity)
-	  }
+    expire.timeLeft -= dt
+    if (expire.timeLeft < 0) {
+      engine.removeEntity(entity)
+    }
   }
 }
