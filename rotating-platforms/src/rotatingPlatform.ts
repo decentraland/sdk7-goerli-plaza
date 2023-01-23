7a63f153271aaa4949b347ec54450472f7637984
William Caine <william.caineiii@gmail.com>
2023-01-23 15:47:39 -0500

    adding rotating platforms

---
+++ rotating-platforms/src/rotatingPlatform.ts
@@ -0,0 +1,21 @@
+//import * as utils from '@dcl/ecs-scene-utils'
+import { engine, Entity, GltfContainer, Transform, TransformType } from '@dcl/sdk/ecs'
+import { Quaternion } from '@dcl/sdk/math'
+import { KeepRotatingSystem, keepRotatingSystem } from './utils/keepRotating'
+
+export function createRotatingPlatform(
+  model: string,
+  transform:Partial<TransformType>,
+  rotation:Quaternion
+): Entity {
+  const entity = engine.addEntity()
+  GltfContainer.create(entity, { src: model })
+  Transform.create(entity, transform)
+
+  //TODO DECIDE PATTERN TO USE
+  //TODO add keep rotating component
+  //keepRotatingSystem.addKeepRotating(entity,rotation);
+  KeepRotatingSystem.instance.addKeepRotating(entity,rotation);
+  
+  return entity
+}
