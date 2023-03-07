45ce3e569586a91483e771e3c73d66f9e5fe7997
nearnshaw <earnshaw.nico@gmail.com>
2023-03-07 17:52:48 -0300

    fix beerdispenser, gnark and block-dog

--- Block-dog/src/index.ts
+++ Block-dog/src/index.ts
@@ -1,29 +1,34 @@
-import { createGLTF } from './gltf'
 import { createDog } from './dog'
-import { InputAction,  pointerEventsSystem, engine } from '@dcl/sdk/ecs'
+import { InputAction,  pointerEventsSystem, engine, Transform, GltfContainer } from '@dcl/sdk/ecs'
 import { changeState, randomSwitchBehavior } from './systems/dogAI'
 import { dogStates } from './components'
 import { moveSystem } from './systems/moveSystem'
 import { Vector3 } from '@dcl/sdk/math'
 export * from '@dcl/sdk'
 
-createGLTF(
-  {
-    position: { x: 8, y: 0, z: 8 },
-    scale: { x: 1.6, y: 1.6, z: 1.6 },
-    rotation: { x: 0, y: 180, z: 0, w: 1 }
-  },
-  'models/garden.glb'
-)
 
-export const bowl = createGLTF(
-  {
-    position: { x: 9, y: 0, z: 1 },
-    scale: { x: 1, y: 1, z: 1 },
-    rotation: { x: 0, y: 180, z: 0, w: 1 }
-  },
-  'models/BlockDogBowl.gltf'
-)
+const garden = engine.addEntity()
+
+Transform.create(garden, {
+    position: Vector3.create(8,0,8),
+    scale: Vector3.create(1.6,1.6,1.6)
+})
+
+GltfContainer.create(garden, {
+  src: 'models/garden.glb'
+})
+
+
+export const bowl = engine.addEntity()
+
+Transform.create(bowl, {
+    position: Vector3.create(9,0,1)
+})
+
+GltfContainer.create(bowl, {
+  src: 'models/BlockDogBowl.gltf'
+})
+
 
 pointerEventsSystem.onPointerDown(
 	bowl, ()=>{
