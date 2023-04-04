d540b92238acb3b7db8c97dcc2d929fda26033f3
menduz <github@menduz.com>
2023-04-04 20:19:25 -0300

    comments and static build

--- build-all.sh
+++ build-all.sh
@@ -2,30 +2,29 @@
 
 echo "Installing and building all folders"
 
+# first install the parametrized package
 if [ ! -z $SDK_VERSION ]; then
   npm i $SDK_VERSION
-  # for d in ./*/ ; do
-  #   if [ -f "$d/scene.json" ]; then
-  #     echo "in $d; no add $SDK_VERSION"
-  #     (cd "$d" && yarn add --silent $SDK_VERSION);
-  #   fi
-  # done
 fi
 
+# then the rest of the dependencies
 npm install
 
+# run a check to ensure all parcels are updated and also dcl-workspace.json
 npm run update-parcels
 
+# and fail if git state is dirty
 git diff --exit-code .
-
 if [[ $? -eq 1 ]]; then
   echo "GIT IS ON DIRTY STATE 🔴 Please run 'npm run update-parcels' locally and commit"
   exit 1
 fi
 
-
 # ensure all packages are on sync
 npm run sync && npm run test
 
-# and build scenes
+# and lastly build scenes
 npm run build
+
+# and export the final static builds
+npm run export-static
