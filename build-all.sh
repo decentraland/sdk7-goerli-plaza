c10e157e26f98a9893be0f2eddf5ade801c9b4f4
menduz <github@menduz.com>
2023-04-04 20:11:15 -0300

    npm

--- build-all.sh
+++ build-all.sh
@@ -4,8 +4,6 @@ echo "Installing and building all folders"
 
 if [ ! -z $SDK_VERSION ]; then
   npm i $SDK_VERSION
-  rm package-lock.json
-  yarn install
   # for d in ./*/ ; do
   #   if [ -f "$d/scene.json" ]; then
   #     echo "in $d; yarn add $SDK_VERSION"
@@ -14,20 +12,20 @@ if [ ! -z $SDK_VERSION ]; then
   # done
 fi
 
-yarn install
+npm install
 
-yarn run update-parcels
+npm run update-parcels
 
 git diff --exit-code .
 
 if [[ $? -eq 1 ]]; then
-  echo "GIT IS ON DIRTY STATE 🔴 Please run 'yarn run update-parcels' locally and commit"
+  echo "GIT IS ON DIRTY STATE 🔴 Please run 'npm run update-parcels' locally and commit"
   exit 1
 fi
 
 
 # ensure all packages are on sync
-yarn run sync && yarn run test
+npm run sync && npm run test
 
 # and build scenes
 npm run build
