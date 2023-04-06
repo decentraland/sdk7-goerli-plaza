8df941f6bab9436df22c77f84607127450f62178
menduz <github@menduz.com>
2023-04-05 21:38:13 -0300

    finishing touches

--- 77,-1-raycast-unit-tests/src/ui.tsx
+++ 77,-1-raycast-unit-tests/src/ui.tsx
@@ -1,26 +1,21 @@
-import ReactEcs, { Label, UiEntity } from "@dcl/sdk/react-ecs"
+import ReactEcs, { Label } from "@dcl/sdk/react-ecs"
 import { Color4 } from "@dcl/sdk/math"
 
 export const ui = () => {
   return (
-    <UiEntity
+    <Label
       uiTransform={{
         width: "100%",
         height: "100%",
         margin: { left: 300 },
-        padding: { top: 10, right: 10, bottom: 10, left: 10 },
+        padding: { top: 100, right: 100, bottom: 100, left: 100 },
       }}
+      value={"This scene tests the raycast using\na testing framework.\nPlease review the console messages."}
+      textAlign="middle-center"
+      fontSize={44}
+      color={Color4.White()}
+      font={'monospace'}
       uiBackground={{ color: { r: 0, g: 0, b: 0, a: 0.8 } }}
-    >
-      <Label
-        uiTransform={{ width: 80, height: 20 }}
-        value={"This scene tests the raycast using a testing framework. Please review the console status."}
-        textAlign="middle-center"
-        fontSize={44}
-        color={Color4.Black()}
-        font={'sans-serif'}
-        uiBackground={{ color: { r: 1, g: 0.45, b: 0.85, a: 1 } }}
-      />
-    </UiEntity>
+    />
   )
 }
