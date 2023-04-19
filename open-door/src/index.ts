5e9c9c45797d0ded21a3183d92ecf6e41ced370c
nearnshaw <earnshaw.nico@gmail.com>
2023-04-19 18:21:29 -0300

    utils library in door and sliding door

--- open-door/src/index.ts
+++ open-door/src/index.ts
@@ -1,42 +1,32 @@
 import {
-  InputAction,
-  pointerEventsSystem,
-  engine,
-  Transform,
-  Entity,
-  MeshRenderer,
-  MeshCollider,
-  Material,
-  Schemas
+	InputAction,
+	pointerEventsSystem,
+	engine,
+	Transform,
+	Entity,
+	MeshRenderer,
+	MeshCollider,
+	Material,
+	Schemas
 } from '@dcl/ecs'
 import { Quaternion, Vector3, Color4 } from '@dcl/ecs-math'
-import { getUserData } from '~system/UserIdentity'
-import { transformComponent } from './system'
 export * from '@dcl/sdk'
-
-getUserData({})
-  .then((value) => console.log(value))
-  .catch((error) => console.log(error))
-
-const DoorComponent = engine.defineComponent('DoorComponent',
-  {
-    open: Schemas.Boolean
-  }
-)
+import * as utils from '@dcl-sdk/utils'
 
 const openPos: Quaternion = Quaternion.create(0, 1, 0)
 const closedPos: Quaternion = Quaternion.create(0, 0, 0)
 
+
 function createWall(position: Vector3, scale: Vector3, parent?: Entity) {
-  const WallEntity = engine.addEntity()
-  Transform.create(WallEntity, {
-    position,
-    scale,
-    parent
-  })
-  MeshRenderer.create(WallEntity, { mesh: { $case: 'box', box: { uvs: [] } } })
-  MeshCollider.create(WallEntity, { mesh: { $case: 'box', box: {} } })
-  return WallEntity
+	const WallEntity = engine.addEntity()
+	Transform.create(WallEntity, {
+		position,
+		scale,
+		parent
+	})
+	MeshRenderer.create(WallEntity, { mesh: { $case: 'box', box: { uvs: [] } } })
+	MeshCollider.create(WallEntity, { mesh: { $case: 'box', box: {} } })
+	return WallEntity
 }
 createWall(Vector3.create(5.75, 1, 3), Vector3.create(1.5, 2, 0.05))
 createWall(Vector3.create(3.25, 1, 3), Vector3.create(1.5, 2, 0.05))
@@ -45,28 +35,33 @@ const doorPivotEntity = engine.addEntity()
 Transform.create(doorPivotEntity, { position: Vector3.create(4, 1, 3), rotation: closedPos })
 
 const doorEntity = createWall(Vector3.create(0.5, 0, 0), Vector3.create(1, 2, 0.05), doorPivotEntity)
-DoorComponent.create(doorEntity, { open: false })
-Material.create(doorEntity, {
-  material: {
-    $case: 'pbr',
-    pbr: {
-      albedoColor: Color4.Red(),
-      metallic: 0.9,
-      roughness: 0.1
-    }
-  }
+
+Material.setPbrMaterial(doorEntity, {
+	albedoColor: Color4.Red(),
+	metallic: 0.9,
+	roughness: 0.1
 })
 
+
+// Add toggle actions to door 
+utils.toggles.addToggle(doorPivotEntity, utils.ToggleState.Off, (value) => {
+	if (value == utils.ToggleState.On) {
+		utils.tweens.startRotation(doorPivotEntity, closedPos, openPos, 0.5)
+	} else {
+		utils.tweens.startRotation(doorPivotEntity, openPos, closedPos, 0.5)
+	}
+})
+
+
+
+
 pointerEventsSystem.onPointerDown(
-  doorEntity,
-  () => {
-    const door = DoorComponent.getMutable(doorEntity)
-    const nextPosition = door.open ? closedPos : openPos
-    door.open = !door.open
-    transformComponent(doorPivotEntity, { rotation: nextPosition }, 0.5)
-  },
-  { 
-	button: InputAction.IA_PRIMARY,
-	hoverText: "Open / Close" 
-  }
+	doorEntity,
+	() => {
+		utils.toggles.flip(doorPivotEntity)
+	},
+	{
+		button: InputAction.IA_PRIMARY,
+		hoverText: "Open / Close"
+	}
 )
--- sliding-door/src/factory.ts
+++ sliding-door/src/factory.ts
@@ -1,39 +1,24 @@
 import {
-  engine, Entity, Material, MeshCollider, MeshRenderer, Transform
+	engine, Entity, Material, MeshCollider, MeshRenderer, Transform
 } from '@dcl/sdk/ecs'
 import { Color4, Vector3 } from '@dcl/sdk/math'
 
 export function createWall(position: Vector3, scale: Vector3, parent?: Entity) {
-  const entity = engine.addEntity()
-  Transform.create(entity, {
-    position,
-    scale,
-    parent
-  })
-  MeshRenderer.create(entity, {
-    mesh: {
-      $case: 'box',
-      box: { uvs: [] }
-    }
-  })
-  MeshCollider.create(entity, {
-    mesh: {
-      $case: 'box',
-      box: {}
-    }
-  })
-  return entity
+	const entity = engine.addEntity()
+	Transform.create(entity, {
+		position,
+		scale,
+		parent
+	})
+	MeshRenderer.setBox(entity)
+	MeshCollider.setBox(entity)
+	return entity
 }
 
 export function setMaterial(entity: Entity) {
-  Material.create(entity, {
-    material: {
-      $case: "pbr",
-      pbr: {
-        albedoColor: Color4.Red(),
-        metallic: 0.9,
-        roughness: 0.1
-      }
-    }
-  })
+	Material.setPbrMaterial(entity, {
+		albedoColor: Color4.Red(),
+		metallic: 0.9,
+		roughness: 0.1
+	})
 }
