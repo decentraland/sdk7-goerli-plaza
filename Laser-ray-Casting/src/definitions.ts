4245fb0a4d4952c06aad420a78e1ba1d0a762d4f
Nicolas Earnshaw <nearnshaw@decentraland.org>
2023-04-21 13:54:04 -0300

    raycast-helper (#45)
    * raycast-helper
    
    * fix versions

--- Laser-ray-Casting/src/definitions.ts
+++ Laser-ray-Casting/src/definitions.ts
@@ -7,32 +7,32 @@ export const speed = 1
 export const rayDistance = 28
 
 export const defaultMaterial: PBMaterial_PbrMaterial = {
-  metallic: 0,
-  roughness: 1,
-  albedoColor: Color4.create(0.2, 0.1, 1)
-}
-export const hitMaterial: PBMaterial_PbrMaterial = {
-  metallic: 1,
-  roughness: 0.5,
-  albedoColor: Color4.create(1, 1, 30)
-}
-export const hitMaterial2: PBMaterial_PbrMaterial = {
-  metallic: 1,
-  roughness: 0.5,
-  albedoColor: Color4.create(0.2, 1, 0.2)
-}
-export const rayMaterial: PBMaterial_PbrMaterial = {
-  metallic: 1,
-  roughness: 0.5,
-  albedoColor: Color4.create(30, 1, 1)
+	metallic: 0,
+	roughness: 1,
+	albedoColor: Color4.create(0.2, 0.1, 1)
 }
 
-export const Ray = engine.defineComponent('Ray',
-  {
-    power: Schemas.Int,
-    timestamp: Schemas.Int
-  }
-)
+export const inmuneMaterial: PBMaterial_PbrMaterial = {
+	metallic: 0,
+	roughness: 1,
+	albedoColor: Color4.Yellow()
+}
+
+export const hitMaterial: PBMaterial_PbrMaterial = {
+	metallic: 1,
+	roughness: 0.5,
+	albedoColor: Color4.create(1, 1, 30)
+}
+export const hitMaterial2: PBMaterial_PbrMaterial = {
+	metallic: 1,
+	roughness: 0.5,
+	albedoColor: Color4.create(0.2, 1, 0.2)
+}
+export const rayMaterial: PBMaterial_PbrMaterial = {
+	metallic: 1,
+	roughness: 0.5,
+	albedoColor: Color4.create(30, 1, 1)
+}
 
 export const MovingCube = engine.defineComponent('MovingCube', {})
 
--- bird-field/src/index.ts
+++ bird-field/src/index.ts
@@ -1,35 +1,41 @@
 export * from '@dcl/sdk'
 import { engine, GltfContainer, Transform } from '@dcl/sdk/ecs'
-import { Vector3 } from '@dcl/sdk/math'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import { spawnBirds } from './modules/birds'
+import * as utils from '@dcl-sdk/utils'
 
 function setup() {
-    // Instantiate ground model
+	// Instantiate ground model
 	const ground = engine.addEntity()
-    GltfContainer.create(ground, {
-        src: 'models/sand.glb'
-    })
+	GltfContainer.create(ground, {
+		src: 'models/sand.glb'
+	})
 
 	// preload the animated bird glbs (underground), for faster loading
 	const birdPreloadDummy = engine.addEntity()
-    GltfContainer.create(birdPreloadDummy, {
-        src: 'models/bird.glb'
-    })
-    Transform.create(birdPreloadDummy, {
-        position: Vector3.create(8, -10, 6)
-    })
+	GltfContainer.create(birdPreloadDummy, {
+		src: 'models/bird.glb'
+	})
+	Transform.create(birdPreloadDummy, {
+		position: Vector3.create(8, -10, 6)
+	})
 
 
-    //  preload the animated bird glbs (underground), for faster loading
-    const birdFlyingPreloadDummy = engine.addEntity()
-    GltfContainer.create(birdFlyingPreloadDummy, {
-        src: 'models/bird_fly.glb'
-    })
-    Transform.create(birdFlyingPreloadDummy, {
-        position: Vector3.create(8, -10, 6)
-    })
+	//  preload the animated bird glbs (underground), for faster loading
+	const birdFlyingPreloadDummy = engine.addEntity()
+	GltfContainer.create(birdFlyingPreloadDummy, {
+		src: 'models/bird_fly.glb'
+	})
+	Transform.create(birdFlyingPreloadDummy, {
+		position: Vector3.create(8, -10, 6)
+	})
 
-	spawnBirds()
+
+	utils.timers.setTimeout(
+		() => {
+			spawnBirds()
+		}, 10000
+	)
 
 }
 
