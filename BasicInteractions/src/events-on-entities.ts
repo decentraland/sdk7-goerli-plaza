5f1e4e6259035eb099f0b5268dff145f87255434
menduz <github@menduz.com>
2023-04-25 18:28:56 -0300

    fix ci

--- BasicInteractions/src/events-on-entities.ts
+++ BasicInteractions/src/events-on-entities.ts
@@ -5,92 +5,88 @@ import { Vector3 } from '@dcl/sdk/math'
 import { paintCube } from './painter'
 import * as utils from '@dcl-sdk/utils'
 
-
 export function setupEventOnEntities() {
+  const pointerDownCube = utils.addTestCube({ position: Vector3.create(2, 1, 4) }, undefined, 'Pointer down')
 
-	const pointerDownCube = utils.addTestCube({ position: Vector3.create(2, 1, 4) }, undefined, 'Pointer down')
+  pointerEventsSystem.onPointerDown(
+    pointerDownCube,
+    function (cmd) {
+      paintCube(pointerDownCube)
+    },
+    {
+      button: InputAction.IA_POINTER,
+      hoverText: 'Activate'
+    }
+  )
 
-	pointerEventsSystem.onPointerDown(
-		pointerDownCube,
-		function (cmd) {
-			paintCube(pointerDownCube)
-		},
-		{
-			button: InputAction.IA_POINTER,
-			hoverText: 'Activate'
-		}
-	)
+  // Pointer Up Cube
 
-	// Pointer Up Cube
+  const pointerUpCube = utils.addTestCube({ position: Vector3.create(2, 1, 6) }, undefined, 'Pointer up')
 
-	const pointerUpCube = utils.addTestCube({ position: Vector3.create(2, 1, 6) }, undefined, 'Pointer up')
+  pointerEventsSystem.onPointerUp(
+    pointerUpCube,
+    function (cmd) {
+      paintCube(pointerUpCube)
+    },
+    {
+      button: InputAction.IA_POINTER,
+      hoverText: 'Activate'
+    }
+  )
 
-	pointerEventsSystem.onPointerUp(
-		pointerUpCube,
-		function (cmd) {
-			paintCube(pointerUpCube)
-		},
-		{
-			button: InputAction.IA_POINTER,
-			hoverText: 'Activate'
-		}
-	)
+  //  Primary Down Cube (while pointing)
+  const primaryDownCube = utils.addTestCube({ position: Vector3.create(8, 1, 12) }, undefined, 'Primary down')
 
-	//  Primary Down Cube (while pointing)
-	const primaryDownCube = utils.addTestCube({ position: Vector3.create(8, 1, 12) }, undefined, 'Primary down')
+  pointerEventsSystem.onPointerDown(
+    primaryDownCube,
+    function (cmd) {
+      paintCube(primaryDownCube)
+    },
+    {
+      button: InputAction.IA_PRIMARY,
+      hoverText: 'Activate'
+    }
+  )
 
+  // Primary Up Cube
+  const primaryUpCube = utils.addTestCube({ position: Vector3.create(10, 1, 12) }, undefined, 'Primary up')
 
-	pointerEventsSystem.onPointerDown(
-		primaryDownCube,
-		function (cmd) {
-			paintCube(primaryDownCube)
-		},
-		{
-			button: InputAction.IA_PRIMARY,
-			hoverText: 'Activate'
-		}
-	)
+  pointerEventsSystem.onPointerUp(
+    primaryUpCube,
+    function (cmd) {
+      paintCube(primaryUpCube)
+    },
+    {
+      button: InputAction.IA_PRIMARY,
+      hoverText: 'Activate'
+    }
+  )
 
-	// Primary Up Cube
-	const primaryUpCube = utils.addTestCube({ position: Vector3.create(10, 1, 12) }, undefined, 'Primary up')
+  // Secondary Down Cube
+  const secondaryDownCube = utils.addTestCube({ position: Vector3.create(12, 1, 12) }, undefined, 'Secondary down')
 
-	pointerEventsSystem.onPointerUp(
-		primaryUpCube,
-		function (cmd) {
-			paintCube(primaryUpCube)
-		},
-		{
-			button: InputAction.IA_PRIMARY,
-			hoverText: 'Activate'
-		}
-	)
+  pointerEventsSystem.onPointerDown(
+    secondaryDownCube,
+    function (cmd) {
+      paintCube(secondaryDownCube)
+    },
+    {
+      button: InputAction.IA_SECONDARY,
+      hoverText: 'Activate'
+    }
+  )
 
-	// Secondary Down Cube
-	const secondaryDownCube = utils.addTestCube({ position: Vector3.create(12, 1, 12) }, undefined, 'Secondary down')
+  // Secondary Up Cube
+  const secondaryUpCube = utils.addTestCube({ position: Vector3.create(14, 1, 12) }, undefined, 'Secondary up')
 
-
-	pointerEventsSystem.onPointerDown(
-		secondaryDownCube,
-		function (cmd) {
-			paintCube(secondaryDownCube)
-		},
-		{
-			button: InputAction.IA_SECONDARY,
-			hoverText: 'Activate'
-		}
-	)
-
-	// Secondary Up Cube
-	const secondaryUpCube = utils.addTestCube({ position: Vector3.create(14, 1, 12) }, undefined, 'Secondary up')
-
-	pointerEventsSystem.onPointerUp(
-		secondaryUpCube,
-		function (cmd) {
-			paintCube(secondaryUpCube)
-		},
-		{
-			button: InputAction.IA_SECONDARY,
-			hoverText: 'Activate'
-		}
-	)
+  pointerEventsSystem.onPointerUp(
+    secondaryUpCube,
+    function (cmd) {
+      paintCube(secondaryUpCube)
+    },
+    {
+      button: InputAction.IA_SECONDARY,
+      hoverText: 'Activate'
+    }
+  )
 }
--- BasicInteractions/src/painter.ts
+++ BasicInteractions/src/painter.ts
@@ -3,15 +3,12 @@ import { Color3 } from '@dcl/sdk/math'
 import * as utils from '@dcl-sdk/utils'
 
 export function paintCube(entity: Entity) {
-	Material.setPbrMaterial(entity, {
-		albedoColor: { a: 1, r: 0, g: 1, b: 0 }
-	})
-	utils.timers.setTimeout(
-		() => {
-			Material.setPbrMaterial(entity, {
-				albedoColor: { a: 1, r: 0, g: 1, b: 1 }
-			})
-		}, 1000
-	)
+  Material.setPbrMaterial(entity, {
+    albedoColor: { a: 1, r: 0, g: 1, b: 0 }
+  })
+  utils.timers.setTimeout(() => {
+    Material.setPbrMaterial(entity, {
+      albedoColor: { a: 1, r: 0, g: 1, b: 1 }
+    })
+  }, 1000)
 }
-
