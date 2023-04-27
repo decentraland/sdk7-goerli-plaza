e8d2fd1be8588f77fbab8b4a8f8551c03b710deb
menduz <github@menduz.com>
2023-04-27 19:10:23 -0300

    main.crdt (#49)
    * main.crdt
    
    * print test result in UI

---
+++ 80,-2-main-crdt/src/index.ts
@@ -0,0 +1,35 @@
+// export all the functions required to make the scene work
+export * from '@dcl/sdk'
+import { GltfContainer, MeshRenderer, Transform, engine } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
+
+import './validations.test'
+
+import { ui } from './ui'
+import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
+
+ReactEcsRenderer.setUiRenderer(ui)
+
+// rotate all cubes
+engine.addSystem(function rotateCube(dt) {
+  for (const [entity] of engine.getEntitiesWith(MeshRenderer)) {
+    const mutableTransform = Transform.getMutable(entity)
+
+    mutableTransform.rotation = Quaternion.multiply(
+      mutableTransform.rotation,
+      Quaternion.fromAngleAxis(dt * 10, Vector3.Up())
+    )
+  }
+})
+
+// rotate all gltf
+engine.addSystem(function rotateCube(dt) {
+  for (const [entity] of engine.getEntitiesWith(GltfContainer)) {
+    const mutableTransform = Transform.getMutable(entity)
+
+    mutableTransform.rotation = Quaternion.multiply(
+      mutableTransform.rotation,
+      Quaternion.fromAngleAxis(dt * 40, Vector3.Up())
+    )
+  }
+})
