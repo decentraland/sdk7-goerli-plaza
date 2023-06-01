29059e3c49c2077ba31c3e29b38c7f7b06443bbd
nearnshaw <earnshaw.nico@gmail.com>
2023-06-01 17:20:15 -0300

    prettier

--- enemy-spawner/src/enemy.ts
+++ enemy-spawner/src/enemy.ts
@@ -1,4 +1,13 @@
-import { Entity, engine, Transform, GltfContainer, AudioSource, PointerEvents, PointerEventType, InputAction } from '@dcl/sdk/ecs'
+import {
+  Entity,
+  engine,
+  Transform,
+  GltfContainer,
+  AudioSource,
+  PointerEvents,
+  PointerEventType,
+  InputAction
+} from '@dcl/sdk/ecs'
 import { Vector3 } from '@dcl/sdk/math'
 import { EnemyShip, Expire, MoveTransformComponent, ShipShapes } from './components/customComponents'
 import { InterpolationType } from './helper/interpolation'
@@ -9,56 +18,56 @@ const SPEED = 2
 const MAX_TRAVEL_SPEED_OFFSET = 0.2
 
 export function spawnEnemy(shape: ShipShapes, x: number, y: number, z: number): Entity {
-	const enemy = engine.addEntity()
+  const enemy = engine.addEntity()
 
-	Transform.create(enemy, {
-		position: Vector3.create(x, y, z)
-	})
+  Transform.create(enemy, {
+    position: Vector3.create(x, y, z)
+  })
 
-	GltfContainer.create(enemy, {
-		src: shape
-	})
+  GltfContainer.create(enemy, {
+    src: shape
+  })
 
-	MoveTransformComponent.create(enemy, {
-		start: { x: x, y: y, z: z },
-		end: { x: x, y: y, z: z - TRAVEL_DISTANCE },
-		speed: SPEED / 10 + Math.random() * (MAX_TRAVEL_SPEED_OFFSET / 10),
-		normalizedTime: 0,
-		lerpTime: 0,
-		hasFinished: false,
-		interpolationType: InterpolationType.EASEINSINE
-	})
+  MoveTransformComponent.create(enemy, {
+    start: { x: x, y: y, z: z },
+    end: { x: x, y: y, z: z - TRAVEL_DISTANCE },
+    speed: SPEED / 10 + Math.random() * (MAX_TRAVEL_SPEED_OFFSET / 10),
+    normalizedTime: 0,
+    lerpTime: 0,
+    hasFinished: false,
+    interpolationType: InterpolationType.EASEINSINE
+  })
 
-	PointerEvents.create(enemy, {
-		pointerEvents: [
-			{
-				eventType: PointerEventType.PET_DOWN,
-				eventInfo: {
-					button: InputAction.IA_PRIMARY,
-					showFeedback: false
-				}
-			}
-		]
-	})
+  PointerEvents.create(enemy, {
+    pointerEvents: [
+      {
+        eventType: PointerEventType.PET_DOWN,
+        eventInfo: {
+          button: InputAction.IA_PRIMARY,
+          showFeedback: false
+        }
+      }
+    ]
+  })
 
-	EnemyShip.create(enemy)
+  EnemyShip.create(enemy)
 
-	return enemy
+  return enemy
 }
 
 export function destroyEnemy(entity: Entity) {
-	MoveTransformComponent.deleteFrom(entity)
-	EnemyShip.deleteFrom(entity)
-	GltfContainer.createOrReplace(entity, {
-		src: 'models/glowingSpaceship.glb'
-	})
-	AudioSource.create(entity, {
-		audioClipUrl: 'sounds/explosion.mp3',
-		playing: true,
-		volume: 2
-	})
+  MoveTransformComponent.deleteFrom(entity)
+  EnemyShip.deleteFrom(entity)
+  GltfContainer.createOrReplace(entity, {
+    src: 'models/glowingSpaceship.glb'
+  })
+  AudioSource.create(entity, {
+    audioClipUrl: 'sounds/explosion.mp3',
+    playing: true,
+    volume: 2
+  })
 
-	Expire.create(entity, {
-		timeLeft: 1
-	})
+  Expire.create(entity, {
+    timeLeft: 1
+  })
 }
--- power-cube/src/powerBase.ts
+++ power-cube/src/powerBase.ts
@@ -1,4 +1,12 @@
-import { AudioSource, engine, GltfContainer, PointerEvents, PointerEventType, Transform, VisibilityComponent } from '@dcl/sdk/ecs'
+import {
+  AudioSource,
+  engine,
+  GltfContainer,
+  PointerEvents,
+  PointerEventType,
+  Transform,
+  VisibilityComponent
+} from '@dcl/sdk/ecs'
 import { Vector3 } from '@dcl/sdk/math'
 import { Particle, particleSystem } from './particles'
 import { createSound } from './sound'
@@ -23,74 +31,73 @@ const powerUp = createSound('sounds/powerUp.mp3')
 const powerDown = createSound('sounds/powerDown.mp3')
 
 export function createPowerBase(position: Vector3, gltfSrc: string) {
-	const entity = engine.addEntity()
+  const entity = engine.addEntity()
 
-	Transform.create(entity, { position })
-	GltfContainer.create(entity, { src: gltfSrc })
-	PointerEvents.create(entity, {
-		pointerEvents: [
-			{
-				eventType: PointerEventType.PET_DOWN,
-				eventInfo: {
-					showFeedback: false
-				}
-			}
-		]
-	})
+  Transform.create(entity, { position })
+  GltfContainer.create(entity, { src: gltfSrc })
+  PointerEvents.create(entity, {
+    pointerEvents: [
+      {
+        eventType: PointerEventType.PET_DOWN,
+        eventInfo: {
+          showFeedback: false
+        }
+      }
+    ]
+  })
 
+  function togglePower(isPowerOn: boolean) {
+    if (isPowerOn) {
+      // TODO: change this workaround until the DisableComponent is available
+      Transform.getMutable(powerBlueGlowEntity).scale = Vector3.One()
+      Transform.getMutable(forcefieldEntity).scale = Vector3.One()
 
-	function togglePower(isPowerOn: boolean) {
-		if (isPowerOn) {
-			// TODO: change this workaround until the DisableComponent is available
-			Transform.getMutable(powerBlueGlowEntity).scale = Vector3.One()
-			Transform.getMutable(forcefieldEntity).scale = Vector3.One()
+      try {
+        engine.addSystem(particleSystem)
+      } catch (err) {}
+      AudioSource.getMutable(powerUp).playing = true
 
-			try {
-				engine.addSystem(particleSystem)
-			} catch (err) { }
-			AudioSource.getMutable(powerUp).playing = true
+      for (const [entity] of engine.getEntitiesWith(Particle)) {
+        VisibilityComponent.deleteFrom(entity)
+      }
+    } else {
+      // NOTE: particles have colliders so need to move them elsewhere
+      for (const [entity] of engine.getEntitiesWith(Particle)) {
+        VisibilityComponent.createOrReplace(entity, { visible: false })
+      }
 
-			for (const [entity] of engine.getEntitiesWith(Particle)) {
-				VisibilityComponent.deleteFrom(entity)
-			}
-		} else {
-			// NOTE: particles have colliders so need to move them elsewhere
-			for (const [entity] of engine.getEntitiesWith(Particle)) {
-				VisibilityComponent.createOrReplace(entity, { visible: false })
-			}
+      // TODO: change this workaround until the DisableComponent is available
+      // Hide the blue glow
+      Transform.getMutable(powerBlueGlowEntity).scale = Vector3.Zero()
+      Transform.getMutable(forcefieldEntity).scale = Vector3.Zero()
 
-			// TODO: change this workaround until the DisableComponent is available
-			// Hide the blue glow
-			Transform.getMutable(powerBlueGlowEntity).scale = Vector3.Zero()
-			Transform.getMutable(forcefieldEntity).scale = Vector3.Zero()
+      engine.removeSystem(particleSystem)
+      AudioSource.getMutable(powerDown).playing = true
+    }
+  }
 
-			engine.removeSystem(particleSystem)
-			AudioSource.getMutable(powerDown).playing = true
-		}
-	}
-
-	utils.triggers.addTrigger(
-		entity,
-		2,
-		2,
-		[
-			{
-				type: 'box',
-				scale: Vector3.create(4, 4, 4),
-				position: Vector3.create(0, 0.75, 0)
-			}
-		],
-		(entity) => {
-			console.log('on enter', { entity })
-			//if (args.length > 0)
-			togglePower(true)
-		},
-		(entity) => {
-			console.log('on exit', { entity })
-			//if (args.length === 0)
-			togglePower(false)
-		}
-	)
+  utils.triggers.addTrigger(
+    entity,
+    2,
+    2,
+    [
+      {
+        type: 'box',
+        scale: Vector3.create(4, 4, 4),
+        position: Vector3.create(0, 0.75, 0)
+      }
+    ],
+    (entity) => {
+      console.log('on enter', { entity })
+      //if (args.length > 0)
+      togglePower(true)
+    },
+    (entity) => {
+      console.log('on exit', { entity })
+      //if (args.length === 0)
+      togglePower(false)
+    }
+  )
 }
 
 // Power base where the power cube sits
