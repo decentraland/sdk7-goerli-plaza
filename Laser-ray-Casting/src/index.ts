e7a896fbdb04ef31f7ef80dbbe050f7315f7b848
nearnshaw <earnshaw.nico@gmail.com>
2023-06-02 17:41:34 -0300

    raycast scenes

--- Laser-ray-Casting/src/index.ts
+++ Laser-ray-Casting/src/index.ts
@@ -1,104 +1,108 @@
 import {
-  ColliderLayer,
-  engine,
-  GltfContainer,
-  Material,
-  MeshCollider,
-  MeshRenderer,
-  RaycastQueryType,
-  raycastSystem,
-  Transform
+	ColliderLayer,
+	engine,
+	GltfContainer,
+	Material,
+	MeshCollider,
+	MeshRenderer,
+	Transform
 } from '@dcl/sdk/ecs'
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
 
 import {
-  boxesCount,
-  defaultMaterial,
-  inmuneMaterial,
-  MovingCube,
-  rayDistance,
-  rayMaterial,
-  RayMesh
+	boxesCount,
+	defaultMaterial,
+	inmuneMaterial,
+	MovingCube,
+	rayDistance,
+	rayMaterial,
+	RayMesh
 } from './definitions'
 import movingCubesSystem from './modules/movingCubes'
 import { createRaycast } from './modules/ray'
 
-export * from '@dcl/sdk'
+export function main() {
 
-const floor = engine.addEntity()
-GltfContainer.create(floor, { src: 'models/Floor_sciFi_03.glb' })
-Transform.create(floor, {
-  position: Vector3.create(16, 0, 16),
-  scale: Vector3.create(2, 0, 2)
-})
+	const floor = engine.addEntity()
+	GltfContainer.create(floor, { src: 'models/Floor_sciFi_03.glb' })
+	Transform.create(floor, {
+		position: Vector3.create(16, 0, 16),
+		scale: Vector3.create(2, 0, 2)
+	})
 
-// laser turret
-const turretBase = engine.addEntity()
-GltfContainer.create(turretBase, { src: 'models/TurretBase_01.glb' })
-Transform.create(turretBase, {
-  position: Vector3.create(16, 0, 2)
-})
+	// laser turret
+	const turretBase = engine.addEntity()
+	GltfContainer.create(turretBase, { src: 'models/TurretBase_01.glb' })
+	Transform.create(turretBase, {
+		position: Vector3.create(16, 0, 2)
+	})
 
-const turretEntity = engine.addEntity()
-GltfContainer.create(turretEntity, { src: 'models/Turret_01.glb' })
-Transform.create(turretEntity, {
-  position: Vector3.create(16, 1, 2),
-  rotation: Quaternion.fromEulerDegrees(0, 0, 0)
-})
-createRaycast(turretEntity)
+	const turretEntity = engine.addEntity()
+	GltfContainer.create(turretEntity, { src: 'models/Turret_01.glb' })
+	Transform.create(turretEntity, {
+		position: Vector3.create(16, 1, 2),
+		rotation: Quaternion.fromEulerDegrees(0, 300, 0)
+	})
 
-// turret constant rotation
-let rotationSpeed = 10
-engine.addSystem((deltaTime) => {
-  const transform = Transform.getMutable(turretEntity)
-  const currentRotation = Quaternion.toEulerAngles(transform.rotation)
-  const yRotation = currentRotation.y + rotationSpeed * deltaTime
-  transform.rotation = Quaternion.fromEulerDegrees(currentRotation.x, yRotation, currentRotation.z)
+	// turret constant rotation
+	let rotationSpeed = 10
+	engine.addSystem((deltaTime) => {
+		const transform = Transform.getMutable(turretEntity)
+		const currentRotation = Quaternion.toEulerAngles(transform.rotation)
+		const yRotation = currentRotation.y + rotationSpeed * deltaTime
+		transform.rotation = Quaternion.fromEulerDegrees(currentRotation.x, yRotation, currentRotation.z)
 
-  if (
-    (rotationSpeed < 0 && yRotation < 336 && yRotation > 300) ||
-    (rotationSpeed > 0 && yRotation > 24 && yRotation < 300)
-  )
-    rotationSpeed *= -1
-})
+		if (
+			(rotationSpeed < 0 && yRotation < 336 && yRotation > 300) ||
+			(rotationSpeed > 0 && yRotation > 24 && yRotation < 300)
+		)
+			rotationSpeed *= -1
+	})
 
-// Ray mesh
-export const rayMeshEntity = engine.addEntity()
-MeshRenderer.setBox(rayMeshEntity)
-Transform.create(rayMeshEntity, {
-  scale: Vector3.create(0.1, 0.1, rayDistance),
-  position: Vector3.create(0, 0.645, rayDistance / 2 + 2),
-  parent: turretEntity
-})
-Material.setPbrMaterial(rayMeshEntity, rayMaterial)
-RayMesh.create(rayMeshEntity)
+	// Ray mesh
+	const rayMeshEntity = engine.addEntity()
+	MeshRenderer.setBox(rayMeshEntity)
+	Transform.create(rayMeshEntity, {
+		scale: Vector3.create(0.1, 0.1, rayDistance),
+		position: Vector3.create(0, 0.645, rayDistance / 2 + 2),
+		parent: turretEntity
+	})
+	Material.setPbrMaterial(rayMeshEntity, rayMaterial)
+	RayMesh.create(rayMeshEntity)
 
-// Moving cubes
-for (let i = 0; i < boxesCount; i++) {
-  createCube(Vector3.create(2 + i * 3, 1.5, 16), true)
-}
 
-// Extra cubes with custom collision layer
-const customLayerCube1 = createCube(Vector3.create(16, 1.5, 20), false)
-MeshCollider.getMutable(customLayerCube1).collisionMask = ColliderLayer.CL_CUSTOM1 | ColliderLayer.CL_PHYSICS
-const customLayerCube2 = createCube(Vector3.create(16, 1.5, 24), false)
-MeshCollider.getMutable(customLayerCube2).collisionMask = ColliderLayer.CL_CUSTOM2
-Material.setPbrMaterial(customLayerCube2, inmuneMaterial)
-const customLayerCube3 = createCube(Vector3.create(16, 1.5, 28), false)
-MeshCollider.getMutable(customLayerCube3).collisionMask = ColliderLayer.CL_CUSTOM3
+	createRaycast(turretEntity, rayMeshEntity)
 
-engine.addSystem(movingCubesSystem)
-//engine.addSystem(raycastResultsSystem)
 
-function createCube(targetPosition: Vector3, movingCube: boolean) {
-  const cube = engine.addEntity()
-  Transform.create(cube, {
-    position: targetPosition
-  })
-  MeshRenderer.setBox(cube)
-  MeshCollider.setBox(cube)
-  Material.setPbrMaterial(cube, defaultMaterial)
-  if (movingCube) MovingCube.create(cube)
+	// Moving cubes
+	for (let i = 0; i < boxesCount; i++) {
+		createCube(Vector3.create(2 + i * 3, 1.5, 16), true)
+	}
 
-  return cube
+	// Extra cubes with custom collision layer
+	const customLayerCube1 = createCube(Vector3.create(16, 1.5, 20), false)
+	MeshCollider.getMutable(customLayerCube1).collisionMask = ColliderLayer.CL_CUSTOM1 | ColliderLayer.CL_PHYSICS
+	const customLayerCube2 = createCube(Vector3.create(16, 1.5, 24), false)
+	MeshCollider.getMutable(customLayerCube2).collisionMask = ColliderLayer.CL_CUSTOM2
+	Material.setPbrMaterial(customLayerCube2, inmuneMaterial)
+	const customLayerCube3 = createCube(Vector3.create(16, 1.5, 28), false)
+	MeshCollider.getMutable(customLayerCube3).collisionMask = ColliderLayer.CL_CUSTOM3
+
+	engine.addSystem(movingCubesSystem)
+	//engine.addSystem(raycastResultsSystem)
+
+	function createCube(targetPosition: Vector3, movingCube: boolean) {
+		const cube = engine.addEntity()
+		Transform.create(cube, {
+			position: targetPosition
+		})
+		MeshRenderer.setBox(cube)
+		MeshCollider.setBox(cube)
+		Material.setPbrMaterial(cube, defaultMaterial)
+		if (movingCube) MovingCube.create(cube)
+
+		return cube
+	}
+
+
 }
--- bird-field/src/index.ts
+++ bird-field/src/index.ts
@@ -2,53 +2,33 @@ export * from '@dcl/sdk'
 import { engine, GltfContainer, GltfContainerLoadingState, LoadingState, Transform } from '@dcl/sdk/ecs'
 import { Vector3 } from '@dcl/sdk/math'
 import { spawnBirds } from './modules/birds'
-import { mendezCoroutineRuntime } from './modules/coroutine'
 
-const corountime = mendezCoroutineRuntime(engine)
+export function main() {
 
-corountime.run(function* waitForAllGtfLoaded() {
-  // Instantiate ground model
-  const ground = engine.addEntity()
-  GltfContainer.create(ground, {
-    src: 'models/sand.glb'
-  })
+	// Instantiate ground model
+	const ground = engine.addEntity()
+	GltfContainer.create(ground, {
+		src: 'models/sand.glb'
+	})
 
-  // preload the animated bird glbs (underground), for faster loading
-  const birdPreloadDummy = engine.addEntity()
-  GltfContainer.create(birdPreloadDummy, {
-    src: 'models/bird.glb'
-  })
-  Transform.create(birdPreloadDummy, {
-    position: Vector3.create(8, -10, 6)
-  })
+	// preload the animated bird glbs (underground), for faster loading
+	const birdPreloadDummy = engine.addEntity()
+	GltfContainer.create(birdPreloadDummy, {
+		src: 'models/bird.glb'
+	})
+	Transform.create(birdPreloadDummy, {
+		position: Vector3.create(8, -10, 6)
+	})
 
-  //  preload the animated bird glbs (underground), for faster loading
-  const birdFlyingPreloadDummy = engine.addEntity()
-  GltfContainer.create(birdFlyingPreloadDummy, {
-    src: 'models/bird_fly.glb'
-  })
-  Transform.create(birdFlyingPreloadDummy, {
-    position: Vector3.create(8, -10, 6)
-  })
+	//  preload the animated bird glbs (underground), for faster loading
+	const birdFlyingPreloadDummy = engine.addEntity()
+	GltfContainer.create(birdFlyingPreloadDummy, {
+		src: 'models/bird_fly.glb'
+	})
+	Transform.create(birdFlyingPreloadDummy, {
+		position: Vector3.create(8, -10, 6)
+	})
 
-  yield // send all updates to renderer
 
-  while (true) {
-    let areLoading = false
-
-    for (const [_entity, loadingState] of engine.getEntitiesWith(GltfContainerLoadingState)) {
-      if (loadingState.currentState == LoadingState.LOADING) {
-        areLoading = true
-        break
-      }
-    }
-
-    if (areLoading) {
-      yield // wait one frame
-    } else {
-      break // finish the loading loop
-    }
-  }
-
-  spawnBirds()
-})
+	spawnBirds()
+}
