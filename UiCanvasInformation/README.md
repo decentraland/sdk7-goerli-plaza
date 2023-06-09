069cb78b25ce07ef735b8c20d807b12a5730e8ef
Pravus <pravusjif@gmail.com>
2023-06-09 17:50:52 +0200

    added scene code

---
+++ UiCanvasInformation/README.md
@@ -0,0 +1,26 @@
+# Ui Canvas Information Scene (SDK7)
+
+Scene to show how the Ui Canvas Information component can be read to change UI element values.
+
+## Try it out
+
+**Install the CLI**
+Download and install the Decentraland CLI by running the following command:
+
+```
+$ npm i -g decentraland
+```
+
+**Previewing the scene**
+
+1) Install the [Decentraland Editor](https://docs.decentraland.org/creator/development-guide/sdk7/editor/)
+
+2) Open a Visual Stuido Code window on the scene's root folder.
+
+3) Open the Decentraland Editor tab, and press **Run Project**
+
+Alternatively, you can use the command line. Inside this scene root directory run:
+
+```
+npm run start
+```
---
+++ UiCanvasInformation/src/index.ts
@@ -0,0 +1,29 @@
+import { engine, UiCanvasInformation } from '@dcl/sdk/ecs'
+import { setupUi } from './ui'
+
+// export all the functions required to make the scene work
+export * from '@dcl/sdk'
+
+export let canvasInfo = {
+  width : 0,
+  height : 0
+}
+
+export function main() {
+  setupUi()
+
+  engine.addSystem((deltaTime) => {
+    const uiCanvasInfo = UiCanvasInformation.getOrNull(engine.RootEntity)
+
+    if(!uiCanvasInfo) return
+
+    canvasInfo.width = uiCanvasInfo.width
+    canvasInfo.height = uiCanvasInfo.height
+
+    // console.log('--------------' +
+    //   '\nscreen width: ' + uiCanvasInfo.width +
+    //   '\nscreen height: ' + uiCanvasInfo.height +
+    //   '\nscreen pixel ratio: ' + uiCanvasInfo.devicePixelRatio +
+    //   '\n--------------')
+  })
+}
