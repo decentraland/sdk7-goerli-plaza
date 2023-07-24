086fb94a55ec177f69ecd68b5b2de95dfd570859
nearnshaw <earnshaw.nico@gmail.com>
2023-07-24 09:30:10 -0300

    turn off debug cubes

--- switchable-platforms/src/switchboard.ts
+++ switchable-platforms/src/switchboard.ts
@@ -11,108 +11,108 @@ Transform.create(switchSound)
 AudioSource.create(switchSound, { audioClipUrl: 'sounds/switch.mp3' })
 
 export function createSwitchBoard(model: string, startPos: Vector3, endPos: Vector3): Entity {
-  const entity = engine.addEntity()
-  GltfContainer.create(entity, { src: model })
-  Transform.create(entity, { position: startPos })
+	const entity = engine.addEntity()
+	GltfContainer.create(entity, { src: model })
+	Transform.create(entity, { position: startPos })
 
-  const buttonA = engine.addEntity()
-  GltfContainer.create(buttonA, { src: 'models/buttonA.glb' })
-  Transform.create(buttonA, { parent: entity })
+	const buttonA = engine.addEntity()
+	GltfContainer.create(buttonA, { src: 'models/buttonA.glb' })
+	Transform.create(buttonA, { parent: entity })
 
-  const buttonB = engine.addEntity()
-  GltfContainer.create(buttonB, { src: 'models/buttonB.glb' })
-  Transform.create(buttonB, { parent: entity })
+	const buttonB = engine.addEntity()
+	GltfContainer.create(buttonB, { src: 'models/buttonB.glb' })
+	Transform.create(buttonB, { parent: entity })
 
-  const gear = engine.addEntity()
-  GltfContainer.create(gear, { src: 'models/gears.glb' })
-  Transform.create(gear, { parent: entity })
+	const gear = engine.addEntity()
+	GltfContainer.create(gear, { src: 'models/gears.glb' })
+	Transform.create(gear, { parent: entity })
 
-  // Height of button when held down
-  const pressedHeight = -0.12
+	// Height of button when held down
+	const pressedHeight = -0.12
 
-  // Add toggle actions to buttons
-  utils.toggles.addToggle(buttonA, utils.ToggleState.Off, (value) => {
-    if (value == utils.ToggleState.On) {
-      Transform.getMutable(buttonA).position.y = pressedHeight
-      Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
-      AudioSource.getMutable(switchSound).playing = true
-    } else {
-      Transform.getMutable(buttonA).position.y = 0
-      Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
-      AudioSource.getMutable(switchSound).playing = true
-    }
-  })
+	// Add toggle actions to buttons
+	utils.toggles.addToggle(buttonA, utils.ToggleState.Off, (value) => {
+		if (value == utils.ToggleState.On) {
+			Transform.getMutable(buttonA).position.y = pressedHeight
+			Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
+			AudioSource.getMutable(switchSound).playing = true
+		} else {
+			Transform.getMutable(buttonA).position.y = 0
+			Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
+			AudioSource.getMutable(switchSound).playing = true
+		}
+	})
 
-  utils.toggles.addToggle(buttonB, utils.ToggleState.Off, (value) => {
-    if (value == utils.ToggleState.On) {
-      Transform.getMutable(buttonB).position.y = pressedHeight
-      Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
-      AudioSource.getMutable(switchSound).playing = true
-    } else {
-      Transform.getMutable(buttonB).position.y = 0
-      Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
-      AudioSource.getMutable(switchSound).playing = true
-    }
-  })
+	utils.toggles.addToggle(buttonB, utils.ToggleState.Off, (value) => {
+		if (value == utils.ToggleState.On) {
+			Transform.getMutable(buttonB).position.y = pressedHeight
+			Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
+			AudioSource.getMutable(switchSound).playing = true
+		} else {
+			Transform.getMutable(buttonB).position.y = 0
+			Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
+			AudioSource.getMutable(switchSound).playing = true
+		}
+	})
 
-  // trigger areas on top of each button
-  utils.triggers.addTrigger(
-    buttonA,
-    utils.LAYER_2,
-    utils.LAYER_1,
-    [{ type: 'box', scale: Vector3.create(2.5, 2.5, 2.5), position: Vector3.create(1.5, 2, 0) }],
-    () => {
-      utils.toggles.set(buttonA, utils.ToggleState.On)
-      movePlatform(entity, gear, -180, endPos)
-    },
-    () => {
-      utils.toggles.set(buttonA, utils.ToggleState.Off)
-    },
-    Color3.Yellow()
-  )
+	// trigger areas on top of each button
+	utils.triggers.addTrigger(
+		buttonA,
+		utils.LAYER_2,
+		utils.LAYER_1,
+		[{ type: 'box', scale: Vector3.create(2.5, 2.5, 2.5), position: Vector3.create(1.5, 2, 0) }],
+		() => {
+			utils.toggles.set(buttonA, utils.ToggleState.On)
+			movePlatform(entity, gear, -180, endPos)
+		},
+		() => {
+			utils.toggles.set(buttonA, utils.ToggleState.Off)
+		},
+		Color3.Yellow()
+	)
 
-  utils.triggers.addTrigger(
-    buttonB,
-    utils.LAYER_2,
-    utils.LAYER_1,
-    [{ type: 'box', scale: Vector3.create(2.5, 2.5, 2.5), position: Vector3.create(-1.5, 2, 0) }],
-    () => {
-      utils.toggles.set(buttonB, utils.ToggleState.On)
-      movePlatform(entity, gear, 180, startPos)
-    },
-    () => {
-      utils.toggles.set(buttonB, utils.ToggleState.Off)
-    },
-    Color3.Yellow()
-  )
+	utils.triggers.addTrigger(
+		buttonB,
+		utils.LAYER_2,
+		utils.LAYER_1,
+		[{ type: 'box', scale: Vector3.create(2.5, 2.5, 2.5), position: Vector3.create(-1.5, 2, 0) }],
+		() => {
+			utils.toggles.set(buttonB, utils.ToggleState.On)
+			movePlatform(entity, gear, 180, startPos)
+		},
+		() => {
+			utils.toggles.set(buttonB, utils.ToggleState.Off)
+		},
+		Color3.Yellow()
+	)
 
-  return entity
+	return entity
 }
 
 function movePlatform(platform: Entity, gear: Entity, rotationSpeed: number, targetPos: Vector3) {
-  utils.tweens.stopTranslation(platform)
+	utils.tweens.stopTranslation(platform)
 
-  utils.perpetualMotions.startRotation(gear, Quaternion.fromEulerDegrees(0, 0, rotationSpeed))
+	utils.perpetualMotions.startRotation(gear, Quaternion.fromEulerDegrees(0, 0, rotationSpeed))
 
-  const currentPos = Transform.get(platform).position
-  const speed = Math.abs(targetPos.x - currentPos.x) * 0.25
+	const currentPos = Transform.get(platform).position
+	const speed = Math.abs(targetPos.x - currentPos.x) * 0.25
 
-  utils.tweens.startTranslation(platform, currentPos, targetPos, speed, utils.InterpolationType.LINEAR, () => {
-    utils.perpetualMotions.stopRotation(gear)
-    Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
-    AudioSource.getMutable(switchSound).playing = true
-  })
+	utils.tweens.startTranslation(platform, currentPos, targetPos, speed, utils.InterpolationType.LINEAR, () => {
+		utils.perpetualMotions.stopRotation(gear)
+		Transform.getMutable(switchSound).position = Transform.get(engine.PlayerEntity).position
+		AudioSource.getMutable(switchSound).playing = true
+	})
 }
 
 // enable debug mode by default in preview mode
 executeTask(async () => {
-  try {
-    const { getRealm } = await import('~system/Runtime')
-    const realm = await getRealm({})
-    if (realm.realmInfo?.isPreview) {
-      utils.triggers.enableDebugDraw(true)
-    }
-  } catch (err) {
-    console.error(err)
-  }
+	try {
+		const { getRealm } = await import('~system/Runtime')
+		const realm = await getRealm({})
+		if (realm.realmInfo?.isPreview) {
+			//utils.triggers.enableDebugDraw(true)
+		}
+	} catch (err) {
+		console.error(err)
+	}
 })
