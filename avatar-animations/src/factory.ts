d1b02ba68b971d7553de09a968f25e0d9b06fd43
Nicolas Earnshaw <nearnshaw@decentraland.org>
2023-07-27 16:47:45 -0300

    avatar animations (#77)

---
+++ avatar-animations/src/factory.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  engine,
+  Transform,
+  MeshRenderer,
+  MeshCollider,
+  PointerEvents,
+  PointerEventType,
+  InputAction,
+  Material
+} from '@dcl/sdk/ecs'
+import { Cube, Spinner } from './components'
+import { Color4 } from '@dcl/sdk/math'
+import { getRandomHexColor } from './utils'
+
+// Cube factory
+export function createCube(x: number, y: number, z: number, spawner = true): Entity {
+  const entity = engine.addEntity()
+
+  // Used to track the cubes
+  Cube.create(entity)
+
+  Transform.create(entity, { position: { x, y, z } })
+
+  // set how the cube looks and collides
+  MeshRenderer.setBox(entity)
+  MeshCollider.setBox(entity)
+  Material.setPbrMaterial(entity, { albedoColor: Color4.fromHexString(getRandomHexColor()) })
+
+  // Make the cube spin, with the circularSystem
+  Spinner.create(entity, { speed: 10 * Math.random() })
+
+  // if it is a spawner, then we set the pointer hover feedback
+  if (spawner) {
+    PointerEvents.create(entity, {
+      pointerEvents: [
+        {
+          eventType: PointerEventType.PET_DOWN,
+          eventInfo: {
+            button: InputAction.IA_PRIMARY,
+            hoverText: 'Press E to spawn',
+            maxDistance: 100,
+            showFeedback: true
+          }
+        }
+      ]
+    })
+  }
+
+  return entity
+}
