26991e85933f3406ab803062df192c388718aee3
nearnshaw <earnshaw.nico@gmail.com>
2023-08-01 17:54:10 -0300

    lint

--- Portal-Puzzle/src/components.ts
+++ Portal-Puzzle/src/components.ts
@@ -2,17 +2,16 @@ import { Schemas, engine } from '@dcl/sdk/ecs'
 
 // Config
 export enum PortalColor {
-	Blue = 0,
-	Orange = 1
+  Blue = 0,
+  Orange = 1
 }
 
 ////////
 
 export const GlowColor = engine.defineComponent('glowColor', { color: Schemas.Number })
 
-
 export const Portal = engine.defineComponent(
-	'portal',
-	{ color: Schemas.EnumNumber<PortalColor>(PortalColor, PortalColor.Blue), coolDown: Schemas.Boolean },
-	{ coolDown: false }
+  'portal',
+  { color: Schemas.EnumNumber<PortalColor>(PortalColor, PortalColor.Blue), coolDown: Schemas.Boolean },
+  { coolDown: false }
 )
--- Portal-Puzzle/src/systems.ts
+++ Portal-Puzzle/src/systems.ts
@@ -1,17 +1,17 @@
 import {
-	engine,
-	Transform,
-	Schemas,
-	inputSystem,
-	InputAction,
-	PointerEventType,
-	Entity,
-	AudioSource,
-	raycastSystem,
-	DeepReadonlyObject,
-	PBRaycastResult,
-	RaycastQueryType,
-	ColliderLayer
+  engine,
+  Transform,
+  Schemas,
+  inputSystem,
+  InputAction,
+  PointerEventType,
+  Entity,
+  AudioSource,
+  raycastSystem,
+  DeepReadonlyObject,
+  PBRaycastResult,
+  RaycastQueryType,
+  ColliderLayer
 } from '@dcl/sdk/ecs'
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import { createPortal } from './portals'
@@ -25,89 +25,85 @@ export let activePortal: PortalColor = PortalColor.Blue
  * switch colors
  */
 export function colorSystem(dt: number) {
-	if (!HOLDING_GUN) return
+  if (!HOLDING_GUN) return
 
-	if (inputSystem.isTriggered(InputAction.IA_PRIMARY, PointerEventType.PET_DOWN)) {
-		if (activePortal == PortalColor.Blue) {
-			activePortal = PortalColor.Orange
-		} else {
-			activePortal = PortalColor.Blue
-		}
+  if (inputSystem.isTriggered(InputAction.IA_PRIMARY, PointerEventType.PET_DOWN)) {
+    if (activePortal == PortalColor.Blue) {
+      activePortal = PortalColor.Orange
+    } else {
+      activePortal = PortalColor.Blue
+    }
 
-		const glowEntities = engine.getEntitiesWith(GlowColor)
-		for (const [entity, glow] of glowEntities) {
-			const glowTranform = Transform.getMutable(entity)
-			if (glow.color == activePortal) {
-				glowTranform.scale = Vector3.One()
-			} else {
-				glowTranform.scale = Vector3.Zero()
-			}
-		}
-	}
+    const glowEntities = engine.getEntitiesWith(GlowColor)
+    for (const [entity, glow] of glowEntities) {
+      const glowTranform = Transform.getMutable(entity)
+      if (glow.color == activePortal) {
+        glowTranform.scale = Vector3.One()
+      } else {
+        glowTranform.scale = Vector3.Zero()
+      }
+    }
+  }
 }
 
 /**
  * create portals
  */
 export function gunSystem(dt: number) {
-	if (!HOLDING_GUN) return
+  if (!HOLDING_GUN) return
 
-	const result = inputSystem.isTriggered(InputAction.IA_POINTER, PointerEventType.PET_DOWN)
+  const result = inputSystem.isTriggered(InputAction.IA_POINTER, PointerEventType.PET_DOWN)
 
-	if (result) {
-
-		raycastSystem.registerGlobalDirectionRaycast(
-			{
-				entity: engine.PlayerEntity,
-				opts: {
-					queryType: RaycastQueryType.RQT_HIT_FIRST,
-					direction: Vector3.rotate(Vector3.Forward(), Transform.get(engine.CameraEntity).rotation),
-					maxDistance: 90,
-					collisionMask: ColliderLayer.CL_PHYSICS | ColliderLayer.CL_POINTER
-				},
-			},
-			function (raycastResult) {
-				handlePortalRay(raycastResult)
-			}
-		)
-	}
+  if (result) {
+    raycastSystem.registerGlobalDirectionRaycast(
+      {
+        entity: engine.PlayerEntity,
+        opts: {
+          queryType: RaycastQueryType.RQT_HIT_FIRST,
+          direction: Vector3.rotate(Vector3.Forward(), Transform.get(engine.CameraEntity).rotation),
+          maxDistance: 90,
+          collisionMask: ColliderLayer.CL_PHYSICS | ColliderLayer.CL_POINTER
+        }
+      },
+      function (raycastResult) {
+        handlePortalRay(raycastResult)
+      }
+    )
+  }
 }
 
-
 export function handlePortalRay(result: DeepReadonlyObject<PBRaycastResult>) {
-	console.log(result)
-	if (result && result.hits[0] && result.hits[0].position && result.hits[0].normalHit) {
+  console.log(result)
+  if (result && result.hits[0] && result.hits[0].position && result.hits[0].normalHit) {
+    // remove old portals of that color
+    const activePortals = engine.getEntitiesWith(Portal)
+    for (const [entity, portal] of activePortals) {
+      if (portal.color == activePortal) {
+        utils.triggers.removeTrigger(entity)
+        engine.removeEntity(entity)
+      }
+    }
 
-		// remove old portals of that color
-		const activePortals = engine.getEntitiesWith(Portal)
-		for (const [entity, portal] of activePortals) {
-			if (portal.color == activePortal) {
-				utils.triggers.removeTrigger(entity)
-				engine.removeEntity(entity)
-			}
-		}
+    const playerPosition = Transform.get(engine.PlayerEntity).position
+    const portalPosition = result.hits[0].position
+    const adjustment = Vector3.scale(Vector3.normalize(Vector3.subtract(playerPosition, portalPosition)), 0.05)
+    const adjustedPortalPosition = Vector3.add(portalPosition, adjustment)
 
-		const playerPosition = Transform.get(engine.PlayerEntity).position
-		const portalPosition = result.hits[0].position
-		const adjustment = Vector3.scale(Vector3.normalize(Vector3.subtract(playerPosition, portalPosition)), 0.05)
-		const adjustedPortalPosition = Vector3.add(portalPosition, adjustment)
+    // create new portal
+    createPortal(activePortal, {
+      position: adjustedPortalPosition,
+      rotation: Quaternion.lookRotation(result.hits[0].normalHit),
 
+      //Quaternion.fromEulerDegrees(result.hit.normalHit.x * 180 / Math.PI, result.hit.normalHit.y * 180 / Math.PI, result.hit.normalHit.z * 180 / Math.PI),
+      scale: Vector3.One()
+    })
 
-		// create new portal
-		createPortal(activePortal, {
-			position: adjustedPortalPosition,
-			rotation: Quaternion.lookRotation(result.hits[0].normalHit),
-
-			//Quaternion.fromEulerDegrees(result.hit.normalHit.x * 180 / Math.PI, result.hit.normalHit.y * 180 / Math.PI, result.hit.normalHit.z * 180 / Math.PI),
-			scale: Vector3.One()
-		})
-
-		//}
-	} else {
-		AudioSource.createOrReplace(engine.PlayerEntity, {
-			audioClipUrl: 'sounds/portalFail.mp3',
-			playing: true,
-			loop: false
-		})
-	}
+    //}
+  } else {
+    AudioSource.createOrReplace(engine.PlayerEntity, {
+      audioClipUrl: 'sounds/portalFail.mp3',
+      playing: true,
+      loop: false
+    })
+  }
 }
