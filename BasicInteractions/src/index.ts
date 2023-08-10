8f4bd2cf4cccbbe94e303bd282e5990998d73b53
nearnshaw <earnshaw.nico@gmail.com>
2023-08-10 18:11:27 -0300

    lint

--- BasicInteractions/src/index.ts
+++ BasicInteractions/src/index.ts
@@ -4,16 +4,21 @@ import { setupProximity } from './proximity'
 import * as utils from '@dcl-sdk/utils'
 import { Color4, Vector3 } from '@dcl/sdk/math'
 import './painter'
+import { setupUi } from './ui'
 
 export function main() {
-  utils.addTestCube(
-    { position: Vector3.create(8, 1, 8), scale: Vector3.create(0.5, 0.5, 0.5) },
-    undefined,
-    "You're using ECS7",
-    Color4.Magenta()
-  )
 
-  setupEventOnEntities()
-  setupProximity()
-  setupGlobalEvents()
+	setupUi()
+
+	utils.addTestCube(
+		{ position: Vector3.create(8, 1, 8), scale: Vector3.create(0.5, 0.5, 0.5) },
+		undefined,
+		"You're using ECS7",
+		Color4.Magenta()
+	)
+
+	setupEventOnEntities()
+	setupProximity()
+	setupGlobalEvents()
 }
+
