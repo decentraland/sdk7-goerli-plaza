e0c607428dc0be0a3f9b22e67e479382baf59bff
nearnshaw <earnshaw.nico@gmail.com>
2023-08-11 09:29:46 -0300

    change package.json

--- auto-dance/src/index.ts
+++ auto-dance/src/index.ts
@@ -1,25 +1,18 @@
-import { Vector3 } from "@dcl/sdk/math";
-import { createDanceArea } from "./autoDance";
-import { engine, Transform, MeshRenderer, GltfContainer } from "@dcl/sdk/ecs";
-
+import { Vector3 } from '@dcl/sdk/math'
+import { createDanceArea } from './autoDance'
+import { engine, Transform, MeshRenderer, GltfContainer } from '@dcl/sdk/ecs'
 
 export function main() {
-
   createDanceArea(Vector3.create(4, 1, 4), Vector3.create(4, 1, 4))
   createDanceArea(Vector3.create(10, 1, 10), Vector3.create(4, 1, 4))
 
-
-
-
   const base = engine.addEntity()
   // Transform.create(base, {
-   
+
   // })
   GltfContainer.create(base, {
-	  src: 'models/baseLight.glb'
-	})
-
-
+    src: 'models/baseLight.glb'
+  })
 
   const platform1 = engine.addEntity()
   Transform.create(platform1, {
@@ -27,9 +20,8 @@ export function main() {
     scale: Vector3.create(2, 1, 2)
   })
   GltfContainer.create(platform1, {
-	  src: 'models/platform.glb'
-	})
-
+    src: 'models/platform.glb'
+  })
 
   const platform2 = engine.addEntity()
   Transform.create(platform2, {
@@ -37,11 +29,6 @@ export function main() {
     scale: Vector3.create(2, 1, 2)
   })
   GltfContainer.create(platform2, {
-	  src: 'models/platform.glb'
-	})
-
-
-
-
-
+    src: 'models/platform.glb'
+  })
 }
