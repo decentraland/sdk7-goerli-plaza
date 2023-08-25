5f424147c2b44617a71bde71d756ab5df57caf91
Bobby Vegas <bobby@vegascity.org>
2023-08-25 07:33:08 +0100

    Added 3 puzzle scenes

---
+++ mirror-puzzle/src/sound.ts
@@ -0,0 +1,33 @@
+import { AudioSource, Entity, Transform, engine } from "@dcl/sdk/ecs"
+
+export class Sound {
+  private sourceEntity: Entity | null = null
+
+  constructor(audioClipUrl: string, loop: boolean) {
+    this.sourceEntity = engine.addEntity()
+
+    Transform.create(this.sourceEntity, {
+      parent: engine.CameraEntity
+    })
+
+    AudioSource.create(this.sourceEntity, {
+      audioClipUrl: audioClipUrl,
+      loop: loop,
+      playing: false
+    })
+  }
+
+  playAudio(): void {
+    if (this.sourceEntity === undefined || this.sourceEntity === null) return
+
+    const audioSource = AudioSource.getMutable(this.sourceEntity)
+    audioSource.playing = true
+  }
+
+  stopAudio(): void {
+    if (this.sourceEntity === undefined || this.sourceEntity === null) return
+
+    const audioSource = AudioSource.getMutable(this.sourceEntity)
+    audioSource.playing = false
+  }
+}
