31fdafd2e6d373cc4a576817c2e80da345f8703d
SaraCG-PM <116716796+SaraCG-PM@users.noreply.github.com>
2023-09-05 14:45:45 +0200

    Physics projects added (#89)
    * Physics projects added
    
    * lint, coordinates, dependency versions, name
    
    ---------
    
    Co-authored-by: nearnshaw <earnshaw.nico@gmail.com>
    Co-authored-by: Nicolas Earnshaw <nearnshaw@decentraland.org>

---
+++ tin-can-alley/src/ui-entities/UiBar.tsx
@@ -0,0 +1,23 @@
+import * as ui from 'dcl-ui-toolkit'
+import { Color4 } from '@dcl/sdk/math'
+
+export const uiBar = ui.createComponent(ui.UIBar, {
+    value: 100,
+    xOffset: -80,
+    yOffset: 80,
+})
+
+const uiIcon = ui.createComponent(ui.SmallIcon, {
+    image:"images/powerIcon.png",
+    width: 90,
+    height: 23,
+    xOffset: -101,
+    yOffset: 85,
+})
+
+uiBar.show()
+uiIcon.show()
+
+uiBar.set(0)
+
+uiBar.color = Color4.Yellow()
