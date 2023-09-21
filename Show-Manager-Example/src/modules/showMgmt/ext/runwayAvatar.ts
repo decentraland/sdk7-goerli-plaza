27ed60cebf0470ee2c9cbd3076623328b1e8b567
James Ashton <james@vegascity.org>
2023-09-21 21:51:00 +0200

    Initial Checkin of Example demonstrating Show Manager for SDK7

---
+++ Show-Manager-Example/src/modules/showMgmt/ext/runwayAvatar.ts
@@ -0,0 +1,176 @@
+import * as showMgmt from 'show-manager/dist'
+import * as utils from '@dcl-sdk/utils'
+import { MeshCollider, MeshRenderer, Transform, engine } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
+
+//FIXME EXTERNALIZE
+export let startPositions = [
+    { position: Vector3.create(1, 1, 1), scale: Vector3.create(1, 1, 1) },
+    { position: Vector3.create(1, 1, 1), scale: Vector3.create(1, 1, 1) },
+]
+
+//FIXME EXTERNALIZE
+export enum RunwayCoord {
+    SOUTH,
+    NORTH
+}
+
+//model animation templates
+
+//models/whiteRabbit_Anim.glb - Walk,Heart_With_Hands,Idle,Run,Wave
+
+const catwalckCube = engine.addEntity()
+MeshRenderer.setBox(catwalckCube)
+MeshCollider.setBox(catwalckCube)
+Transform.create(catwalckCube, {
+    position: Vector3.create(8, 0, 10),
+    scale: Vector3.create(3, 1.421, 8)
+})
+
+// Coordinates for main Runway Walk
+const point1 = Vector3.create(12.9, 0.725, 2.89)
+const point2 = Vector3.create(10.13, 0.725, 4.92)
+const point3 = Vector3.create(8, 0.725, 6.05)
+const point4 = Vector3.create(8.16, 0.725, 13.28)
+const path1: Vector3[] = [point1, point2, point3, point4]
+
+// Coordinates for Return Walk
+const point5 = Vector3.create(8.16, 0.725, 13.28)
+const point6 = Vector3.create(8, 0.725, 6.05)
+const point7 = Vector3.create(5.81, 0.725, 5.19)
+const point8 = Vector3.create(3.54, 0.725, 3.24)
+const path2: Vector3[] = [point5, point6, point7, point8]
+
+const path3: Vector3[] = [point8, point7, point6, point5]
+const path4: Vector3[] = [point4, point3, point2, point1]
+/*
+const path3 = path2.reverse()
+const path4 = path1.reverse()*/
+//model animation templates
+
+//FIXME EXTERNALIZE
+export let runwayPaths: any = {
+    //starting south
+    0: {
+        0: {
+            duration: 4,
+            path: path1
+        },
+        1: {
+            duration: 4,
+            path: path2
+        }
+    },
+
+    //starting north
+    1: {
+        0: {
+            duration: 8,
+            path: path3
+        },
+        1: {
+            duration: 8,
+            path: path4
+        }
+    }
+}
+
+//export let portalAnims = ["Flower.Open", "Flower.Close"]
+
+const CLASSNAME = "RunwayAvatar"
+export class RunwayAvatar extends showMgmt.ShowEntityModel {
+
+    id: string
+    start: number
+    poseIndex = 0
+    //FIXME EXTERNALIZE
+    poseDuration = [3333, 7500, 9000, 7500, 3333] //30 fps
+    runwayPosition = -1
+    walkAnim!: string
+    poseAnims: any
+    portalPosition = -1
+
+    originalStart: number
+    originalPosition: Vector3
+
+    constructor(id: string, model: string, invisible: boolean, start: number, position: Vector3, scale: Vector3, idelAnim?: string) {
+        super(model, {
+            idleAnim: idelAnim,
+            startInvisible: invisible,
+            transform: {
+                position: position,
+                rotation: Quaternion.Identity(),
+                scale: scale
+            }
+        })
+        this.id = id
+        this.start = start
+        this.originalStart = start
+        this.originalPosition = Vector3.clone(position)
+    }
+
+    reset() {
+        const METHOD_NAME = "reset"
+        console.log(CLASSNAME, METHOD_NAME, this.id, 'ENTRY')
+        //TODO move to parent class to reset all stuff
+        this.stopAllAnimations()
+        this.start = this.originalStart
+        this.poseIndex = 0
+        this.runwayPosition = -1
+        this.portalPosition = -1
+
+        let transform = Transform.createOrReplace(this.entity)
+
+        if (this.originalPosition) {
+            transform.position = Vector3.clone(this.originalPosition)
+        }
+
+        utils.paths.stopPath(this.entity)
+    }
+
+    startModel(sequence: string[]) {
+        const METHOD_NAME = "startModel"
+        console.log(CLASSNAME, METHOD_NAME, this.id, 'ENTRY')
+
+        this.appear()
+
+        this.walkAnim = sequence.splice(0, 1).toString()
+        this.poseAnims = sequence
+
+        this.runSequence()
+    }
+
+    runSequence() {
+        const METHOD_NAME = "runSequence"
+        console.log(CLASSNAME, METHOD_NAME, this.id, 'ENTRY')
+        this.runwayPosition++
+        if (this.runwayPosition < this.poseAnims.length) {
+            this.playAnimation(this.walkAnim, false, undefined, undefined, undefined, true)
+
+            console.log(CLASSNAME, METHOD_NAME, this.id, 'RUNWAY POSITION IS', this.runwayPosition, this.poseIndex)
+
+            utils.paths.startSmoothPath(this.entity, runwayPaths[this.start][this.runwayPosition].path, runwayPaths[this.start][this.runwayPosition].duration, 60, true, () => {
+                if (this.runwayPosition == this.poseAnims.length - 1) {
+                    console.log(CLASSNAME, METHOD_NAME, this.id, 'remove model from engine')
+                    this.hide()
+                    this.stopAllAnimations()
+                }
+                else {
+                    this.playAnimation(this.poseAnims[this.poseIndex], true, undefined, undefined, undefined, true)
+
+                    console.log(CLASSNAME, METHOD_NAME, this.id, 'pausing for pose', this.poseIndex, this.poseAnims[this.poseIndex], this.poseDuration[this.poseIndex])
+
+                    const self = this
+                    utils.timers.setTimeout(
+                        function () {
+                            console.log(CLASSNAME, METHOD_NAME, self.id, 'pose over', self.poseIndex, self.poseAnims[self.poseIndex], self.poseDuration[self.poseIndex])
+                            self.poseIndex++
+                            self.runSequence()
+                        },
+                        this.poseDuration[this.poseIndex]
+                    )
+                }
+            })
+        }
+    }
+}
---
+++ Show-Manager-Example/src/modules/showStatusDisplays.ts
@@ -0,0 +1,185 @@
+import * as showMgmt from 'show-manager/dist'
+import { Entity, Font, TextShape, Transform, engine } from '@dcl/sdk/ecs'
+import { Color3, Quaternion, Vector3 } from '@dcl/sdk/math'
+import { VideoScreens } from './videoScreens'
+
+export abstract class ShowStatusDisplays {
+    static messageBoard: Entity
+    static currentShow: Entity
+
+    static Initialise() {
+        ShowStatusDisplays.messageBoard = engine.addEntity()
+        Transform.create(ShowStatusDisplays.messageBoard, {
+            parent: VideoScreens.S1,
+            position: Vector3.create(0, 0, 0.2),
+            rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+            scale: Vector3.create(0.25, (0.25 / 9) * 16, 0.25)
+        })
+        TextShape.create(ShowStatusDisplays.messageBoard, {
+            text: "",
+            fontSize: 1.6,
+            font: Font.F_SERIF,
+            textWrapping: true,
+            width: 1.8,
+            outlineColor: Color3.Black(),
+            outlineWidth: 0.05
+        })
+
+        /// current show
+        ShowStatusDisplays.currentShow = engine.addEntity()
+        Transform.create(ShowStatusDisplays.currentShow, {
+            position: Vector3.create(8, 7.7, 8),
+            rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+            scale: Vector3.create(2, 2, 2)
+        })
+        TextShape.create(ShowStatusDisplays.currentShow, {
+            text: "",
+            fontSize: 4,
+            textWrapping: true,
+            width: 15,
+            outlineColor: Color3.Black(),
+            outlineWidth: 0.2
+        })
+    }
+
+    static setBoardMessage(text: string) {
+        let messageText = TextShape.getMutableOrNull(ShowStatusDisplays.messageBoard)
+        if (messageText) {
+            messageText.text = text
+        }
+    }
+
+    static hideBoard() {
+        let messageText = TextShape.getMutableOrNull(ShowStatusDisplays.messageBoard)
+        if (messageText) {
+            messageText.text = ""
+        }
+        CountDownTimer.terminate()
+    }
+
+    static startNextShowCounter(runOfShow: showMgmt.ShowType[]) {
+        console.log('STARTING NEW COUNTER, ', runOfShow)
+        let currentTime = Date.now() / 1000
+        let nextShow: showMgmt.ShowType | undefined
+        for (let show of runOfShow) {
+            console.log(show.artist, ' STARTS IN ', show.startTime ? show.startTime - currentTime : "---")
+            if (show && show.startTime && show.startTime - currentTime > 0) {
+                if (nextShow && nextShow !== undefined && nextShow.startTime) {
+                    if (show.startTime - currentTime < nextShow.startTime - currentTime) {
+                        nextShow = show
+                    }
+                } else {
+                    nextShow = show
+                }
+            }
+        }
+    
+        if (CountDownTimer.instance) {
+            CountDownTimer.instance = null
+        }
+    
+        if (!nextShow) {
+            this.setBoardMessage("That's all for today on this stage. See you tomorrow!")
+            return
+        }
+    
+        console.log(
+            'IDENTIFIED NEXT SHOW, ',
+            nextShow,
+            ' STARTING IN ',
+            (nextShow.startTime) ? nextShow.startTime - currentTime : "---"
+        )
+    
+        // contdown w nextShow
+    
+        CountDownTimer.createAndAddToEngine(nextShow)
+    
+        // TODO if last show over "return tomorrow"
+    }
+
+    static setArtistName(name?: string) {
+        let currentShowText = TextShape.getMutableOrNull(ShowStatusDisplays.currentShow)
+        if (currentShowText) {
+            if (name && name !== undefined) {
+                currentShowText.text = name
+            } else {
+                currentShowText.text = ""
+            }
+        }
+    }
+    
+    static hideArtistName() {
+        let currentShowText = TextShape.getMutableOrNull(ShowStatusDisplays.currentShow)
+        if (currentShowText) {
+            currentShowText.text = ""
+        }
+    }
+}
+
+export class CountDownTimer {
+    static instance: CountDownTimer | null = null
+
+    timeToEvent = 60 * 5
+    secondsTimer = 1
+    artistName: string = ''
+    constructor(show: showMgmt.ShowType) {
+        console.log('SHOW STARTS AT ', show.startTime, ' NOW IS ', Date.now() / 1000)
+        if (!show) throw new Error("show must not be null")
+        if (!show.startTime) throw new Error("show.startTime must not be null")
+        this.timeToEvent = show.startTime - Date.now() / 1000
+        this.artistName = show.artist ? show.artist : ""
+    }
+
+    static createAndAddToEngine(show: showMgmt.ShowType): CountDownTimer {
+        if (CountDownTimer.instance == null) {
+            CountDownTimer.instance = new CountDownTimer(show)
+            engine.addSystem(CountDownTimer.update)
+        }
+        return CountDownTimer.instance
+    }
+
+    static terminate() {
+        if (CountDownTimer.instance) {
+            engine.removeSystem(CountDownTimer.update)
+            CountDownTimer.instance = null
+        }
+    }
+
+    static update(dt: number) {
+        if (CountDownTimer.instance === undefined || CountDownTimer.instance === null) return
+
+        CountDownTimer.instance.update(dt)
+    }
+
+    update(dt: number) {
+        if (this.timeToEvent > 0) {
+            this.secondsTimer -= dt
+            this.timeToEvent -= dt
+
+            if (this.secondsTimer < 0) {
+                this.secondsTimer = 1
+                let timeAsString = secondsToString(this.timeToEvent)
+                ShowStatusDisplays.setBoardMessage(this.artistName + '\nstarts in just\n' + timeAsString)
+            }
+        } else {
+            console.log('show about to start!')
+            ShowStatusDisplays.hideBoard()
+        }
+    }
+}
+
+function secondsToString(rawSeconds: number) {
+    let seconds = (Math.floor(rawSeconds) % 60).toString()
+    let minutes = (Math.floor(rawSeconds / 60) % 60).toString()
+    let hours = Math.floor(rawSeconds / 3600).toString()
+
+    if (seconds.length == 1) {
+        seconds = '0' + seconds
+    }
+
+    if (minutes.length == 1) {
+        minutes = '0' + minutes
+    }
+
+    return hours + ':' + minutes + ':' + seconds
+}
