686dbaf0abea5d4a3764285c563a2f80f0276b85
nearnshaw <earnshaw.nico@gmail.com>
2023-10-03 10:29:30 -0300

    lint

--- avatar-swap/src/modules/arissa.ts
+++ avatar-swap/src/modules/arissa.ts
@@ -2,29 +2,29 @@ import { engine, Entity, GltfContainer, Transform, Animator } from '@dcl/sdk/ecs
 import { Vector3 } from '@dcl/sdk/math'
 
 export function createArissaCharacter(): Entity {
-	const parentEntity = engine.addEntity()
-	const entity = engine.addEntity()
+  const parentEntity = engine.addEntity()
+  const entity = engine.addEntity()
 
-	GltfContainer.create(entity, {
-		src: 'models/arissa.glb'
-	})
-	Transform.create(entity, {
-		position: Vector3.create(0, 1.75, 0),
-		scale: Vector3.create(0, 0, 0),
-		parent: parentEntity
-	})
-	Animator.create(entity, {
-		states: [
-			{
-				clip: 'Running',
-				loop: true
-			},
-			{
-				clip: 'Idle',
-				loop: true
-			}
-		]
-	})
+  GltfContainer.create(entity, {
+    src: 'models/arissa.glb'
+  })
+  Transform.create(entity, {
+    position: Vector3.create(0, 1.75, 0),
+    scale: Vector3.create(0, 0, 0),
+    parent: parentEntity
+  })
+  Animator.create(entity, {
+    states: [
+      {
+        clip: 'Running',
+        loop: true
+      },
+      {
+        clip: 'Idle',
+        loop: true
+      }
+    ]
+  })
 
-	return entity
+  return entity
 }
