f667c00af4300d06e3e422752f1d499133ad2e9f
nearnshaw <earnshaw.nico@gmail.com>
2023-10-03 12:28:19 -0300

    minor animator refactor

--- Block-Fountain/src/ring.ts
+++ Block-Fountain/src/ring.ts
@@ -1,12 +1,11 @@
-import { Entity, engine, Transform, GltfContainer, Animator } from "@dcl/sdk/ecs";
-import { Quaternion, Vector3 } from "@dcl/sdk/math";
-
+import { Entity, engine, Transform, GltfContainer, Animator } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
 
 export class Ring {
-  private ringEntity: Entity;
-  animation1: string;
-  animation2: string;
-  animation3: string;
+  private ringEntity: Entity
+  animation1: string
+  animation2: string
+  animation3: string
 
   constructor(
     position: Vector3,
@@ -18,75 +17,65 @@ export class Ring {
     animation3: string,
     parent: Entity
   ) {
-    
     // Create an entity for the ring
-    this.ringEntity = engine.addEntity();
+    this.ringEntity = engine.addEntity()
 
     // Calculate entity rotation in Euler degrees
-    const eulerRotationRing = Quaternion.fromEulerDegrees(
-      rotation.x,
-      rotation.y,
-      rotation.z
-    );
+    const eulerRotationRing = Quaternion.fromEulerDegrees(rotation.x, rotation.y, rotation.z)
 
     // Add a 3D model to the entity
     GltfContainer.create(this.ringEntity, {
-        src: model
-    });
+      src: model
+    })
 
     // Add a transform and parent
     Transform.create(this.ringEntity, {
-        position: position,
-        rotation: eulerRotationRing,
-        scale: scale,
-        parent: parent
-    });
+      position: position,
+      rotation: eulerRotationRing,
+      scale: scale,
+      parent: parent
+    })
 
     // Initialize animation names
-    this.animation1 = animation1;
-    this.animation2 = animation2;
-    this.animation3 = animation3;
+    this.animation1 = animation1
+    this.animation2 = animation2
+    this.animation3 = animation3
 
     // Create an animator for the ring with multiple animation states
     Animator.create(this.ringEntity, {
-        states: [
-          {
-              name: animation1,
-              clip: animation1,
-              playing: false,
-              loop: false
+      states: [
+        {
+          clip: animation1,
+          playing: false,
+          loop: false
         },
         {
-              name: animation2,
-              clip: animation2,
-              playing: false,
-              loop: false
+          clip: animation2,
+          playing: false,
+          loop: false
         },
         {
-              name: animation3,
-              clip: animation3,
-              playing: false,
-              loop: false
+          clip: animation3,
+          playing: false,
+          loop: false
         }
-      ] 
-    });
-}
+      ]
+    })
+  }
 
   public play1(): void {
     // Play the first animation
-    Animator.playSingleAnimation(this.ringEntity, this.animation1);
-    console.log('Playing Animation 1');
-   
+    Animator.playSingleAnimation(this.ringEntity, this.animation1)
+    console.log('Playing Animation 1')
   }
   public play2(): void {
     // Play the second animation
-    Animator.playSingleAnimation(this.ringEntity, this.animation2);
-    console.log('Playing Animation 2');
-
+    Animator.playSingleAnimation(this.ringEntity, this.animation2)
+    console.log('Playing Animation 2')
   }
   public play3(): void {
     // Play the third animation
-    Animator.playSingleAnimation(this.ringEntity, this.animation3);
-    console.log('Playing Animation 3');
+    Animator.playSingleAnimation(this.ringEntity, this.animation3)
+    console.log('Playing Animation 3')
   }
 }
