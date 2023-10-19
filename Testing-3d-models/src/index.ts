8a0fb8057435b7a7b6a49796cccf35e84e526053
nearnshaw <earnshaw.nico@gmail.com>
2023-10-19 12:22:47 -0300

    retouch "testing 3d models"

--- Testing-3d-models/src/index.ts
+++ Testing-3d-models/src/index.ts
@@ -1,10 +1,5 @@
-import { engine, executeTask, GltfContainer, Material, Transform } from '@dcl/sdk/ecs'
-import { Color4, Vector3 } from '@dcl/sdk/math'
-
-import { createCube } from './factory'
-import { bounceScalingSystem, circularSystem, spawnerSystem } from './systems'
-
-import { setupUi } from './ui'
+import { engine, GltfContainer, Transform } from '@dcl/sdk/ecs'
+import { Vector3 } from '@dcl/sdk/math'
 
 // export all the functions required to make the scene work
 export * from '@dcl/sdk'
@@ -14,8 +9,8 @@ let samplescene = engine.addEntity()
 GltfContainer.create(samplescene, { src: 'models/SampleScene.glb' })
 
 Transform.create(samplescene, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let samplescene_02 = engine.addEntity()
@@ -23,8 +18,8 @@ let samplescene_02 = engine.addEntity()
 GltfContainer.create(samplescene_02, { src: 'models/SampleScene_02.glb' })
 
 Transform.create(samplescene_02, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let samplescene_03 = engine.addEntity()
@@ -32,8 +27,8 @@ let samplescene_03 = engine.addEntity()
 GltfContainer.create(samplescene_03, { src: 'models/SampleScene_03.glb' })
 
 Transform.create(samplescene_03, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let animatedcube = engine.addEntity()
@@ -41,8 +36,8 @@ let animatedcube = engine.addEntity()
 GltfContainer.create(animatedcube, { src: 'models/AnimatedCube.glb' })
 
 Transform.create(animatedcube, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let animatedcubemorph = engine.addEntity()
@@ -50,8 +45,8 @@ let animatedcubemorph = engine.addEntity()
 GltfContainer.create(animatedcubemorph, { src: 'models/AnimatedCubeMorph.glb' })
 
 Transform.create(animatedcubemorph, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let animatedspheremorph = engine.addEntity()
@@ -59,8 +54,8 @@ let animatedspheremorph = engine.addEntity()
 GltfContainer.create(animatedspheremorph, { src: 'models/AnimatedSphereMorph.glb' })
 
 Transform.create(animatedspheremorph, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let riggedanimation = engine.addEntity()
@@ -68,8 +63,8 @@ let riggedanimation = engine.addEntity()
 GltfContainer.create(riggedanimation, { src: 'models/RiggedAnimation.glb' })
 
 Transform.create(riggedanimation, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let riggeddracoanimation = engine.addEntity()
@@ -77,8 +72,8 @@ let riggeddracoanimation = engine.addEntity()
 GltfContainer.create(riggeddracoanimation, { src: 'models/RiggedDracoAnimation.glb' })
 
 Transform.create(riggeddracoanimation, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
 
 let avocado = engine.addEntity()
@@ -86,6 +81,6 @@ let avocado = engine.addEntity()
 GltfContainer.create(avocado, { src: 'models/avocado.glb' })
 
 Transform.create(avocado, {
-  position: Vector3.create(32, 0, 32),
-  scale: Vector3.create(1, 1, 1)
+	position: Vector3.create(32, 0, 32),
+	scale: Vector3.create(1, 1, 1)
 })
