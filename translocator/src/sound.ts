29ce0bdfb36749f95125b666b71676caa43ea19d
Kevin Sy <kevin.sy@msn.com>
2023-10-26 14:20:18 -0700

    Translocator first commit (#121)
    * box2d-ball example scene first commit
    
    * first commit
    
    * Fix disc not going directly to player
    
    * versions and lint
    
    * parcels and library versions
    
    * version
    
    * lint
    
    * delete box2d scene
    
    * lint
    
    ---------
    
    Signed-off-by: Nicolas Earnshaw <nearnshaw@decentraland.org>
    Co-authored-by: nearnshaw <earnshaw.nico@gmail.com>
    Co-authored-by: Nicolas Earnshaw <nearnshaw@decentraland.org>

---
+++ translocator/src/sound.ts
@@ -0,0 +1,21 @@
+import { AudioSource, AvatarAnchorPointType, AvatarAttach, Entity, Transform, engine } from '@dcl/sdk/ecs'
+
+export class Sound {
+  entity: Entity
+
+  constructor(audioUrl: string) {
+    this.entity = engine.addEntity()
+    Transform.create(this.entity)
+
+    AudioSource.create(this.entity, {
+      audioClipUrl: audioUrl,
+      loop: false,
+      playing: false
+    })
+
+    AvatarAttach.create(this.entity, {
+      // Play the sound wherever the player is standing
+      anchorPointId: AvatarAnchorPointType.AAPT_POSITION
+    })
+  }
+}
