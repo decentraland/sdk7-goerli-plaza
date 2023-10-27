5c62e713e49977e1fcfefdf05c98af1552fa7296
Bence Varga <dzsunyec@gmail.com>
2023-10-27 17:05:51 +0200

    ui sprite code added

---
+++ ui-spritesheet/src/index.ts
@@ -0,0 +1,11 @@
+import { engine } from '@dcl/sdk/ecs'
+
+import { setupUi, SpriteAnimSystem } from './ui'
+
+
+engine.addSystem(SpriteAnimSystem)
+
+export function main() {
+  // draw UI
+  setupUi()
+}
