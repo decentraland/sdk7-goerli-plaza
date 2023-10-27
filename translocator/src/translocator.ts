2d80c8cfe41a4aa26d81ef0af4034527569cec81
nearnshaw <earnshaw.nico@gmail.com>
2023-10-27 09:45:49 -0300

    translocator hide

--- translocator/src/translocator.ts
+++ translocator/src/translocator.ts
@@ -1,45 +1,48 @@
 import { Entity, GltfContainer, Transform, TransformType, engine } from '@dcl/sdk/ecs'
 import { Vector3 } from '@dcl/sdk/math'
+import { OnlyInScene } from './onlyRenderInScene'
 
 export class Translocator {
-  entity: Entity = engine.addEntity()
-  isFired: boolean = false
-  blueGlow: Entity = engine.addEntity()
-  orangeGlow: Entity = engine.addEntity()
+	entity: Entity = engine.addEntity()
+	isFired: boolean = false
+	blueGlow: Entity = engine.addEntity()
+	orangeGlow: Entity = engine.addEntity()
 
-  constructor(transform: TransformType) {
-    Transform.create(this.entity, transform)
-    GltfContainer.create(this.entity, {
-      src: 'models/translocator.glb'
-    })
+	constructor(transform: TransformType) {
+		Transform.create(this.entity, transform)
+		GltfContainer.create(this.entity, {
+			src: 'models/translocator.glb'
+		})
 
-    // Glow setup
-    Transform.create(this.blueGlow, {
-      parent: this.entity
-    })
-    GltfContainer.create(this.blueGlow, {
-      src: 'models/blueGlow.glb'
-    })
+		// Glow setup
+		Transform.create(this.blueGlow, {
+			parent: this.entity
+		})
+		GltfContainer.create(this.blueGlow, {
+			src: 'models/blueGlow.glb'
+		})
 
-    Transform.create(this.orangeGlow, {
-      parent: this.entity
-    })
-    GltfContainer.create(this.orangeGlow, {
-      src: 'models/orangeGlow.glb'
-    })
+		Transform.create(this.orangeGlow, {
+			parent: this.entity
+		})
+		GltfContainer.create(this.orangeGlow, {
+			src: 'models/orangeGlow.glb'
+		})
 
-    this.setGlow(false)
-  }
+		OnlyInScene.create(this.entity)
 
-  // Switches between the glows
-  setGlow(isFired: boolean): void {
-    this.isFired = isFired
-    if (isFired) {
-      Transform.getMutable(this.blueGlow).scale = Vector3.Zero()
-      Transform.getMutable(this.orangeGlow).scale = Vector3.One()
-    } else {
-      Transform.getMutable(this.blueGlow).scale = Vector3.One()
-      Transform.getMutable(this.orangeGlow).scale = Vector3.Zero()
-    }
-  }
+		this.setGlow(false)
+	}
+
+	// Switches between the glows
+	setGlow(isFired: boolean): void {
+		this.isFired = isFired
+		if (isFired) {
+			Transform.getMutable(this.blueGlow).scale = Vector3.Zero()
+			Transform.getMutable(this.orangeGlow).scale = Vector3.One()
+		} else {
+			Transform.getMutable(this.blueGlow).scale = Vector3.One()
+			Transform.getMutable(this.orangeGlow).scale = Vector3.Zero()
+		}
+	}
 }
