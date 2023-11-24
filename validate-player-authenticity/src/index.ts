962f2f80d23a1c514999134b4164fa1076f6068d
nearnshaw <earnshaw.nico@gmail.com>
2023-11-24 13:46:31 -0300

    multiline and validate player scene

--- validate-player-authenticity/src/index.ts
+++ validate-player-authenticity/src/index.ts
@@ -1,49 +1,58 @@
 import { InputAction, executeTask, pointerEventsSystem } from '@dcl/sdk/ecs'
 import { Scene } from './environment'
 import { signedFetch } from '~system/SignedFetch'
-import { Announcement } from './ui'
+import { setupUi } from './ui'
+import * as ui from 'dcl-ui-toolkit'
 
 export function main() {
-  const scene = new Scene()
-  const ui = new Announcement()
+	const scene = new Scene()
 
-  pointerEventsSystem.onPointerDown(
-    {
-      entity: scene.theFountainOfBrokenDreams,
-      opts: { button: InputAction.IA_POINTER, hoverText: 'Is your heart pure?' }
-    },
-    () => {
-      executeTask(async () => {
-        try {
-          let response = await signedFetch({
-            url: 'http://localhost:8080/check-validity',
-            init: {
-              headers: { 'Content-Type': 'application/json' },
-              method: 'GET'
-            }
-          })
+	setupUi()
 
-          if (!response.body) {
-            throw new Error('Invalid response')
-          }
 
-          let json = await JSON.parse(response.body)
+	pointerEventsSystem.onPointerDown(
+		{
+			entity: scene.theFountainOfBrokenDreams,
+			opts: { button: InputAction.IA_POINTER, hoverText: 'Is your heart pure?' }
+		},
+		() => {
+			executeTask(async () => {
+				try {
+					let response = await signedFetch({
+						url: 'http://localhost:8080/check-validity',
+						init: {
+							headers: { 'Content-Type': 'application/json' },
+							method: 'GET'
+						}
+					})
 
-          console.log('Response received: ', json)
+					if (!response.body) {
+						throw new Error('Invalid response')
+					}
 
-          if (!json.valid) {
-            throw new Error('Does not pass validation check')
-          }
+					let json = await JSON.parse(response.body)
 
-          scene.passedValidation()
-          ui.passedAnnouncement.show()
-        } catch (error) {
-          console.log(error)
+					console.log('Response received: ', json)
 
-          scene.failedValidation()
-          ui.failedAnnouncement.show()
-        }
-      })
-    }
-  )
+					if (!json.valid) {
+						throw new Error('Does not pass validation check')
+					}
+
+					scene.passedValidation()
+					const announce = ui.createComponent(ui.Announcement, {
+						value: 'Your intentions are pure', duration: 5, startHidden: false
+					})
+					announce.show()
+				} catch (error) {
+					console.log(error)
+
+					scene.failedValidation()
+					const announce = ui.createComponent(ui.Announcement, {
+						value: 'Your dark schemes are not welcome here, be gone!', duration: 5, startHidden: false
+					})
+
+				}
+			})
+		}
+	)
 }
