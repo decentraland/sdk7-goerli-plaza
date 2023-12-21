347a1c16b0273bc361ad02ae30104d0f42d55229
nearnshaw <earnshaw.nico@gmail.com>
2023-12-21 11:45:11 -0300

    clean up and lint

--- Simple-NFT/src/factory.ts
+++ Simple-NFT/src/factory.ts
@@ -9,26 +9,18 @@ import {
   InputAction,
   Material
 } from '@dcl/sdk/ecs'
-import { Cube, Spinner } from './components'
+
 import { Color4 } from '@dcl/sdk/math'
-import { getRandomHexColor } from './utils'
 
 // Cube factory
 export function createCube(x: number, y: number, z: number, spawner = true): Entity {
   const entity = engine.addEntity()
-
-  // Used to track the cubes
-  Cube.create(entity)
 
   Transform.create(entity, { position: { x, y, z } })
 
   // set how the cube looks and collides
   MeshRenderer.setBox(entity)
   MeshCollider.setBox(entity)
-  Material.setPbrMaterial(entity, { albedoColor: Color4.fromHexString(getRandomHexColor()) })
-
-  // Make the cube spin, with the circularSystem
-  Spinner.create(entity, { speed: 100 * Math.random() })
 
   // Create PointerEvent with the hover feedback.
   // We are going to check the onClick event on the changeColorSystem.
--- Simple-NFT/src/index.ts
+++ Simple-NFT/src/index.ts
@@ -1,26 +1,46 @@
-import { InputAction, MeshCollider, MeshRenderer, Transform, engine, pointerEventsSystem } from "@dcl/sdk/ecs";
-import { Vector3 } from "@dcl/sdk/math";
-import { MakeNFTFrame, createFullNFT, displayNFTUI } from "./Resources/nftUI";
+import {
+  InputAction,
+  MeshCollider,
+  MeshRenderer,
+  NftFrameType,
+  NftShape,
+  Transform,
+  engine,
+  pointerEventsSystem
+} from '@dcl/sdk/ecs'
+import { Color4, Vector3 } from '@dcl/sdk/math'
+import { MakeNFTFrame, createFullNFT, displayNFTUI } from './Resources/nftUI'
+import { setupUi } from './ui'
 
-const cube = engine.addEntity()
-Transform.create(cube , {
-    position : Vector3.create(5,1,5)
-})
-MeshRenderer.setBox(cube)
-MeshCollider.setBox(cube)
+export function main() {
+  setupUi()
 
-pointerEventsSystem.onPointerDown(
+  // Shortcut to make an NFT picture frame
+  MakeNFTFrame('0x07ccfd0fbada4ac3c22ecd38037ca5e5c0ad8cfa', '48', 8, 1, 8, 1, 1, 1, 0, 0, 0)
+
+  // Shortcut to make an NFT picture frame that is also clickable and includes a UI
+  createFullNFT('0x07ccfd0fbada4ac3c22ecd38037ca5e5c0ad8cfa', '48', 10, 1, 10, 1, 1, 1, 0, 0, 0)
+
+  // Manually create an NFT picture frame that is also clickable and includes a UI
+  const manualNFT = engine.addEntity()
+  Transform.create(manualNFT, {
+    position: Vector3.create(5, 1, 5)
+  })
+  NftShape.create(manualNFT, {
+    urn: 'urn:decentraland:ethereum:erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:229795',
+    color: Color4.Red(),
+    style: NftFrameType.NFT_GOLD_CARVED
+  })
+  pointerEventsSystem.onPointerDown(
     {
-        entity : cube ,
-        opts : {
-            button : InputAction.IA_POINTER,
-            hoverText : "Click Here"
-        }
-    }, 
+      entity: manualNFT,
+      opts: {
+        button: InputAction.IA_POINTER,
+        hoverText: 'Click Here'
+      }
+    },
     function () {
-        displayNFTUI("0x07ccfd0fbada4ac3c22ecd38037ca5e5c0ad8cfa","48")
+      displayNFTUI('0x06012c8cf97bead5deae237070f9587f8e7a266d', '229795')
     }
-)
-
-MakeNFTFrame("0x07ccfd0fbada4ac3c22ecd38037ca5e5c0ad8cfa","48",8,1,8,1,1,1,0,0,0)
-createFullNFT("0x07ccfd0fbada4ac3c22ecd38037ca5e5c0ad8cfa","48",10,1,10,1,1,1,0,0,0)
+  )
+}
