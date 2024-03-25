b7afc134cc32a6a92b5d962c5f0f65a03a4906de
nearnshaw <earnshaw.nico@gmail.com>
2024-03-25 12:08:46 -0300

    remove initAssetPacks

--- Editor-actions/src/index.ts
+++ Editor-actions/src/index.ts
@@ -1,33 +1,3 @@
-import {
-  Animator,
-  AudioSource,
-  AudioStream,
-  AvatarAttach,
-  engine,
-  GltfContainer,
-  Material,
-  pointerEventsSystem,
-  Transform,
-  UiBackground,
-  UiText,
-  UiTransform,
-  VideoPlayer,
-  VisibilityComponent
-} from '@dcl/sdk/ecs'
-import { initAssetPacks } from '@dcl/asset-packs/dist/scene-entrypoint'
-
-initAssetPacks(engine, pointerEventsSystem, {
-  Animator,
-  AudioSource,
-  AvatarAttach,
-  Transform,
-  VisibilityComponent,
-  GltfContainer,
-  Material,
-  VideoPlayer,
-  UiTransform,
-  UiText,
-  UiBackground
-})
+import {} from '@dcl/sdk/ecs'
 
 export function main() {}
