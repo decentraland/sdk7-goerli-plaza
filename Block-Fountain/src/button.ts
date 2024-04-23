13e39f406e66f4f7f7326d2683d027705f52870c
nearnshaw <earnshaw.nico@gmail.com>
2024-04-23 13:01:35 -0300

    use new playSound syntax

--- Block-Fountain/src/button.ts
+++ Block-Fountain/src/button.ts
@@ -12,7 +12,7 @@ import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import { ECS6ComponentAudioSource } from '~system/EngineApi'
 
 export class ButtonClickComponent {
-  constructor(public clickAnim: string, public audioSource: string) {}
+  constructor(public clickAnim: string, public audioSource: string) { }
 }
 
 export class Button {
@@ -84,8 +84,7 @@ export class Button {
     Animator.playSingleAnimation(this.buttonEntity, this.buttonClickComponent.clickAnim)
 
     // Fetch the button's audio source and play it
-    const buttonSource = AudioSource.getMutable(this.buttonEntity)
-    buttonSource.playing = true
+    AudioSource.playSound(this.buttonEntity, this.buttonClickComponent.audioSource)
   }
 }
 
