2aa5827c318b30594a74ae246686151e241526d4
nearnshaw <earnshaw.nico@gmail.com>
2024-04-25 15:10:22 -0300

    comment out zombie killer cooldown

--- Zombie-shooter/src/systems/zombieKiller.ts
+++ Zombie-shooter/src/systems/zombieKiller.ts
@@ -4,17 +4,17 @@ import { playSound } from '../systems/sound'
 import { engine, InputAction, inputSystem, PointerEventType, Transform } from '@dcl/sdk/ecs'
 import { GameControllerComponent } from '../components/gameController'
 
-const _COOLDOWN = 0.5
-
-var coolDown = 0
+// const _COOLDOWN = 0.5
+// var coolDown = 0
 
 export function zombieKiller(dt: number) {
-  coolDown -= dt
+  // coolDown -= dt
 
   const result = inputSystem.getInputCommand(InputAction.IA_POINTER, PointerEventType.PET_DOWN)
-  if (result && coolDown <= 0) {
+  if (result) {
+    // && coolDown <= 0) {
     playSound(engine.CameraEntity, 'sounds/explosion.mp3', true)
-    coolDown = _COOLDOWN
+    // coolDown = _COOLDOWN
 
     for (const [zombieEntity] of engine.getEntitiesWith(ZombieComponent)) {
       if (result.hit?.entityId === zombieEntity) {
