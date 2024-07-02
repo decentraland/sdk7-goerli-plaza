a1346036ed49e3ee169d8655c1cd21ff09c05b09
Lean Mendoza <leandro@decentraland.org>
2024-07-02 15:01:41 -0300

    fix validate-player-authenticity

--- validate-player-authenticity/server/src/index.ts
+++ validate-player-authenticity/server/src/index.ts
@@ -15,20 +15,21 @@ const app = express()
 
 app.use(cors({ origin: true }))
 
-app.get(
-  '/check-validity',
-  dclExpress({ expiration: VALID_SIGNATURE_TOLERANCE_INTERVAL_MS }),
-  async (req: Request & dcl.DecentralandSignatureData<Metadata>, res: Response) => {
-    req.baseUrl = SERVER_BASE_URL
-    try {
-      await runChecks(req, VALID_PARCEL)
-      return res.status(200).send({ valid: true, msg: 'Valid request' })
-    } catch (error) {
-      console.log(error)
-      return res.status(400).send({ valid: false, error: `Can't validate your request` })
-    }
+app.get('/check-validity', async (req: Request & dcl.DecentralandSignatureData<Metadata>, res: Response) => {
+  try {
+    req.baseUrl = SERVER_BASE_URL;
+    return dcl.express({ expiration: VALID_SIGNATURE_TOLERANCE_INTERVAL_MS })(req, res, async () => {
+      try {
+        await runChecks(req, VALID_PARCEL);
+        return res.status(200).send({ valid: true, msg: 'Valid request' });
+      } catch (error) {
+        return res.status(400).send({ valid: false, error: error });
+      }
+    });
+  } catch (error) {
+    return res.status(400).send({ valid: false, error: error });
   }
-)
+});
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on port ${SERVER_PORT} - base URL: "${SERVER_BASE_URL}"`);
