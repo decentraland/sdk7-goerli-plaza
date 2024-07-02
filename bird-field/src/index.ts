b8f2135a2c3374ffdb9b608c0543e861d7615b5b
nearnshaw <earnshaw.nico@gmail.com>
2024-07-02 13:29:31 -0300

    place ground via visual editor

--- bird-field/src/index.ts
+++ bird-field/src/index.ts
@@ -15,7 +15,7 @@ import { setupUi } from './ui'
 
 // please do not remove this coroutine, it exists to test the compliance of the GltfContainerLoadingState component
 const corountime = mendezCoroutineRuntime(engine)
-let entitiesToWait : Entity[]
+let entitiesToWait: Entity[]
 
 
 function logCurrentMoment(log: string) {
@@ -26,15 +26,10 @@ function logCurrentMoment(log: string) {
 corountime.run(function* waitForAllGtfLoaded() {
   logCurrentMoment(`initializing coroutine`)
 
-  const ground = engine.addEntity()
-  GltfContainer.create(ground, {
-    src: 'models/sand.glb'
-  })
-
   // preload the animated bird glbs (underground), for faster loading
   const birdPreloadDummy = engine.addEntity()
   GltfContainer.create(birdPreloadDummy, {
-    src: 'models/bird.glb'
+    src: 'assets/scene/bird.glb'
   })
   Transform.create(birdPreloadDummy, {
     position: Vector3.create(8, -10, 6)
@@ -43,14 +38,14 @@ corountime.run(function* waitForAllGtfLoaded() {
   //  preload the animated bird glbs (underground), for faster loading
   const birdFlyingPreloadDummy = engine.addEntity()
   GltfContainer.create(birdFlyingPreloadDummy, {
-    src: 'models/bird_fly.glb'
+    src: 'assets/scene/bird_fly.glb'
   })
   Transform.create(birdFlyingPreloadDummy, {
     position: Vector3.create(8, -10, 6)
   })
-  
-  entitiesToWait = [ground, birdPreloadDummy, birdFlyingPreloadDummy]
-  
+
+  entitiesToWait = [birdPreloadDummy, birdFlyingPreloadDummy]
+
   yield* waitForAllModelsToLoad(engine)
 
   logCurrentMoment('spawningBirds')
