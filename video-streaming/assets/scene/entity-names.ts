83518a3636b699ee03a7816d44eecfa070196620
nearnshaw <earnshaw.nico@gmail.com>
2025-05-06 10:40:25 +0200

    fix gitignore

---
+++ video-streaming/assets/scene/entity-names.ts
@@ -0,0 +1,14 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  Admin_Tools = "Admin Tools",
+  Video_Screen = "Video Screen",
+  baseDarkWithCollider_glb = "baseDarkWithCollider.glb",
+  screenBody = "screenBody",
+  seats = "seats",
+  sreenDisplay = "sreenDisplay",
+} 
