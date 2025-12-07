11f7c7f380f794aa8739b1da980bc58b9a4451ed
NickKhalow <nickhalow@gmail.com>
2025-12-07 06:12:08 +0200

    update deps and add scaling

--- audio-visualization/src/factory.ts
+++ audio-visualization/src/factory.ts
@@ -13,6 +13,7 @@ import { VisualAmplitude, VisualBar } from './components'
 import { Color4 } from '@dcl/sdk/math'
 
 export function createVisualBar(x: number, y: number, z: number, index: number): Entity {
+  console.log("createVisualAmplitude: start for " + index)
   const entity = engine.addEntity()
 
   // Used to react to audio bands
@@ -20,23 +21,17 @@ export function createVisualBar(x: number, y: number, z: number, index: number):
 
   Transform.create(entity, { position: { x, y, z } })
 
-  // set how the cube looks and collides
   MeshRenderer.setBox(entity)
   MeshCollider.setBox(entity)
   Material.setPbrMaterial(entity, { albedoColor: Color4.Green() })
 
-  // Create PointerEvent with the hover feedback.
-  // We are going to check the onClick event on the changeColorSystem.
-  PointerEvents.create(entity, {
-    pointerEvents: [
-      { eventType: PointerEventType.PET_DOWN, eventInfo: { button: InputAction.IA_POINTER, hoverText: 'Change Color' } }
-    ]
-  })
+  console.log("createVisualAmplitude: finish for " + index)
 
   return entity
 }
 
 export function createVisualAmplitude(x: number, y: number, z: number): Entity {
+  console.log("createVisualAmplitude: start")
   const entity = engine.addEntity()
 
   // Used to react to audio amplitude
@@ -44,10 +39,11 @@ export function createVisualAmplitude(x: number, y: number, z: number): Entity {
 
   Transform.create(entity, { position: { x, y, z } })
 
-  // set how the cube looks and collides
-  MeshRenderer.setSphere(entity)
-  MeshCollider.setSphere(entity)
+  MeshRenderer.setBox(entity)
+  MeshCollider.setBox(entity)
   Material.setPbrMaterial(entity, { albedoColor: Color4.Yellow() })
+
+  console.log("createVisualAmplitude: finish")
 
   return entity
 }
--- audio-visualization/src/index.ts
+++ audio-visualization/src/index.ts
@@ -14,7 +14,11 @@ import { createVisualBar, createVisualAmplitude } from './factory'
 
 const BANDS: number = 8
 
+const BARS_HEIGHT: number = 8;
+const AMPLITUDE_SCALE : number = 4;
+
 export function main() {
+  console.log("Init")
   const currentAnalysis: AudioAnalysisView = { amplitude: 0, bands: new Array<number>(BANDS) }
 
   const audioEntity = engine.addEntity()
@@ -23,16 +27,19 @@ export function main() {
     playing: true,
     loop: true
   })
-  AudioAnalysis.createAudioAnalysis(audioEntity, undefined, undefined, undefined)
+  AudioAnalysis.createAudioAnalysis(audioEntity)
+  Transform.create(audioEntity)
 
   for (let i = 0; i < BANDS; i++) {
     createVisualBar(5, 0, i, i)
   }
 
-  createVisualAmplitude(0, 0, 0)
+  createVisualAmplitude(0, 5, 0)
 
   // Read
-  engine.addSystem(() => AudioAnalysis.readIntoView(audioEntity, currentAnalysis))
+  engine.addSystem(() => {
+    AudioAnalysis.readIntoView(audioEntity, currentAnalysis)
+  })
 
   // Bands
   engine.addSystem(() => {
@@ -42,7 +49,7 @@ export function main() {
       const index = VisualBar.get(entity).index
 
       const current = mutableTransform.position
-      current.y = currentAnalysis.bands[index]
+      current.y = currentAnalysis.bands[index] * BARS_HEIGHT;
       mutableTransform.position = current
     }
   })
@@ -53,7 +60,7 @@ export function main() {
     for (const [entity, _spinner, _transform] of entities) {
       const mutableTransform = Transform.getMutable(entity)
 
-      const value = currentAnalysis.amplitude
+      const value = currentAnalysis.amplitude * AMPLITUDE_SCALE;
       mutableTransform.scale = Vector3.create(value, value, value)
     }
   })
