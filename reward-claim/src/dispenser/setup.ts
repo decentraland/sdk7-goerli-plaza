fa9c0bb8de796b28434b89ba264aa0f1b24aa35a
Pejosonic <bobrow@gmail.com>
2026-01-08 20:24:02 -0300

    feat: reward-claim add updated dispenser model

--- reward-claim/src/dispenser/setup.ts
+++ reward-claim/src/dispenser/setup.ts
@@ -4,25 +4,37 @@ import { ClaimConfig } from "./claiming/claimConfig"
 import { Vector3, Quaternion } from "@dcl/sdk/math"
 import { initDispenserUi } from "./claiming/ui/dispenserUi"
 
-export function setupDispenser(){
-    
+export function setupDispenser() {
+
     initDispenserUi()
-    if (Date.now() <= new Date("2025-11-03 00:00 UTC").getTime()) {
-        
-        const dispenserBase = createDispenser(
-            ClaimConfig.campaign.CAMPAIGN_TEST, 
-            ClaimConfig.campaign.CAMPAIGN_TEST.campaignKeys.key, 
-            {
-                position: { x: 4, y: 0, z: 8 },
-                rotation: Quaternion.fromEulerDegrees(0, 150, 0),
-                scale: Vector3.scale(Vector3.One(), 1.1)
-            },
-            'models/dispenser/Dispenser.glb',
-            'models/dispenser/Button.glb',
-            'Button',
-            'models/dispenser/balloons.glb',
-            true
-        )
-    }
-    
+
+    const dispenserBase = createDispenser(
+        ClaimConfig.campaign.CAMPAIGN_TEST,
+        ClaimConfig.campaign.CAMPAIGN_TEST.campaignKeys.key,
+        {
+            position: { x: 4, y: 0, z: 8 },
+            rotation: Quaternion.fromEulerDegrees(0, 150, 0),
+            scale: Vector3.scale(Vector3.One(), 1.1)
+        },
+        'models/dispenser/Dispenser.glb',
+        'models/dispenser/Button.glb',
+        'Button',
+        'models/dispenser/balloons.glb',
+        true
+    )
+
+    const reward = engine.addEntity()
+
+    Transform.create(reward, {
+        position: Vector3.create(0, 0, 0),
+        scale: Vector3.create(1, 1, 1),
+        parent: dispenserBase
+    })
+
+    GltfContainer.create(reward, {
+        src: 'models/dispenser/DCLWearableShow.glb',
+        visibleMeshesCollisionMask: undefined,
+        invisibleMeshesCollisionMask: undefined
+    })
+
 }
