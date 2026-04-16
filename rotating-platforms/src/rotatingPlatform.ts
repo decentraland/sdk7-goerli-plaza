53041ff9802f06aaa3a11a4e0d26cde3c623536c
Pravus <pravusjif@gmail.com>
2026-04-16 21:11:28 +0200

    corrected rotating tunnel platform axis

--- rotating-platforms/src/rotatingPlatform.ts
+++ rotating-platforms/src/rotatingPlatform.ts
@@ -32,22 +32,22 @@ export function createRotatingPlatform(
 
   switch (direction) {
     case Direction.X:
-      directionQuat = Quaternion.fromEulerDegrees(0, 0, 90)
+      directionQuat = Quaternion.fromEulerDegrees(-90, 0, 0)
       break
     case Direction.invX:
-      directionQuat = Quaternion.fromEulerDegrees(0, 0, -90)
+      directionQuat = Quaternion.fromEulerDegrees(90, 0, 0)
       break
     case Direction.Y:
       directionQuat = Quaternion.fromEulerDegrees(0, 90, 0)
       break
     case Direction.invY:
-      directionQuat = Quaternion.fromEulerDegrees(0, -90, 0)
+      directionQuat = Quaternion.fromEulerDegrees(0, 90, 0)
       break
     case Direction.Z:
-      directionQuat = Quaternion.fromEulerDegrees(90, 0, 0)
+      directionQuat = Quaternion.fromEulerDegrees(0, 0, 90)
       break
     case Direction.invZ:
-      directionQuat = Quaternion.fromEulerDegrees(-90, 0, 0)
+      directionQuat = Quaternion.fromEulerDegrees(0, 0, -90)
       break
   }
 
