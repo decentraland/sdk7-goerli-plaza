f0a536bd78f301947faf8a909eea6c57550c0333
nearnshaw <earnshaw.nico@gmail.com>
2026-07-28 17:39:27 -0300

    replace vimeo videos

--- video-streaming/src/index.ts
+++ video-streaming/src/index.ts
@@ -28,6 +28,6 @@ export function main() {
 
 
 // Other video links:
-//'https://player.vimeo.com/external/878776484.m3u8?s=0b62be8cfb1d35f8bf30fcb33170a6f3a86620fe&logging=false'
+//'https://vz-8a0704eb-552.b-cdn.net/bf77e515-7998-4040-85be-c863b5c65111/playlist.m3u8'
 
-//'https://player.vimeo.com/external/552481870.m3u8?s=c312c8533f97e808fccc92b0510b085c8122a875'
+//'https://vz-8a0704eb-552.b-cdn.net/868d1f35-9036-4985-9a3f-73d4498213a5/playlist.m3u8'
